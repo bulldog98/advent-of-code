a67cf1e362f6a686eafda9443ec1080e95977e69
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2021-12-01 20:18:23 +0100

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.fold(-1 to -1) { (sum, last), curr ->
+            val increase = if (last < curr) 1 else 0
+            (sum + increase)  to curr
+        }.first
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(3).fold(-1 to -1) { (sum, last), (a, b, c) ->
+            val curr = a + b + c
+            val increase = if (last < curr) 1 else 0
+            val newSum = sum + increase
+            newSum to curr
+        }.first
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
