879d24e42a877d0b1104578ae36a4d2147915a4f
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2021-12-10 11:55:43 +0100

    Solve Day09

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -3,20 +3,28 @@ package day09
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int =
-        input.size
+    fun part1(input: List<String>): Int {
+        val heightMap = HeightMap(input)
+        return heightMap.lowPoints.sumOf { heightMap[it] + 1 }
+    }
 
-    fun part2(input: List<String>): Int =
-        input.size
+    fun part2(input: List<String>): Int {
+        val heightMap = HeightMap(input)
+        return heightMap.lowPoints
+            .map { it.computeBasin(heightMap).size }
+            .sortedDescending()
+            .take(3)
+            .fold(1, Int::times)
+    }
 
     val testInput = readInput("day09/Day09_test")
     val input = readInput("day09/Day09")
 
     // test if implementation meets criteria from the description:
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 15)
     println(part1(input))
 
     // test if implementation meets criteria from the description:
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 1134)
     println(part2(input))
 }
