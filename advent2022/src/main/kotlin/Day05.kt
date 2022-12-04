36ce857dfa71fa7e06e35f7c5f5b228c949fa040
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-04 20:42:32 +0100

    Prepare Day05

---
+++ advent2022/src/main/kotlin/Day05.kt
@@ -0,0 +1,7 @@
+class Day05 : AdventDay<Int>(2022, 5) {
+    override fun part1(input: List<String>): Int = TODO()
+
+    override fun part2(input: List<String>): Int = TODO()
+}
+
+fun main() = Day05().run()
---
+++ advent2022/src/test/kotlin/Day05Test.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+    private val day = Day05()
+
+    @Test
+    fun part1() = assertEquals(2, day.testPart1())
+
+
+    @Test
+    fun part2() = assertEquals(4, day.testPart2())
+}
