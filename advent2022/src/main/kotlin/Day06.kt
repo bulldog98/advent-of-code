57c7e5f77b2d58efae709b4b7f274da26ffd2a4d
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-05 19:42:55 +0100

    Prepare for Day06

---
+++ advent2022/src/main/kotlin/Day06.kt
@@ -0,0 +1,7 @@
+class Day06 : AdventDay<String>(2022, 6) {
+    override fun part1(input: List<String>): String = TODO("Not yet implemented")
+    override fun part2(input: List<String>): String = TODO("Not yet implemented")
+}
+
+
+fun main() = Day06().run()
---
+++ advent2022/src/test/kotlin/Day06Test.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+    private val day = Day06()
+
+    @Test
+    fun part1() = assertEquals("CMZ", day.testPart1())
+
+
+    @Test
+    fun part2() = assertEquals("MCD", day.testPart2())
+}
