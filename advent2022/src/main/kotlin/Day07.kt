461447d6167c8c916a5d53336c37d25a92297565
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-06 06:09:29 +0100

    Prepare for Day07

---
+++ advent2022/src/main/kotlin/Day07.kt
@@ -0,0 +1,7 @@
+class Day07 : AdventDay<Int>(2022, 6) {
+    override fun part1(input: List<String>): Int = TODO()
+    override fun part2(input: List<String>): Int = TODO()
+}
+
+
+fun main() = Day07().run()
