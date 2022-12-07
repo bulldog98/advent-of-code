6f3c7300b64c4958eea210279aebb2efcd0c1e25
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-07 18:52:34 +0100

    Perpare Day08

---
+++ advent2022/src/main/kotlin/Day08.kt
@@ -0,0 +1,7 @@
+class Day08 : AdventDay<Int>(2022, 8) {
+    override fun part1(input: List<String>): Int = input.size
+    override fun part2(input: List<String>): Int = input.size
+}
+
+
+fun main() = Day08().run()
---
+++ advent2022/src/test/kotlin/Day08Test.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+    private val day = Day08()
+
+    @Test
+    fun part1() = assertEquals(1, day.testPart1())
+
+
+    @Test
+    fun part2() = assertEquals(1, day.testPart2())
+}
