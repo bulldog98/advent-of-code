6b4cca98f033a6b0e076ede4908313a39377c180
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-07 19:49:46 +0100

    Improve Solution for Day07 after watching stream

--- advent2022/src/main/kotlin/Day07.kt
+++ advent2022/src/main/kotlin/Day07.kt
@@ -1,87 +1,69 @@
-sealed interface Instruction
-object Ls : Instruction {
-    override fun toString(): String = "Ls"
+sealed interface Node {
+    val name: String
+    val parent: Node?
+    val fileSize: Int
 }
-object Up : Instruction {
-    override fun toString(): String = "Up"
+
+class File(override val fileSize: Int, override val name: String, override val parent: Node?): Node
+class Directory(
+    override val name: String,
+    override val parent: Node?,
+    val children: MutableList<Node> = mutableListOf()
+): Node {
+    override val fileSize: Int
+        get() = children.sumOf { it.fileSize }
+
+    fun filterInTree(pre: (Directory) -> Boolean): List<Directory> =
+        children.filterIsInstance<Directory>()
+            .flatMap { it.filterInTree(pre) } +
+                if (pre(this))
+                    listOf(this)
+                else
+                    emptyList()
 }
-data class Cd(val dir: String): Instruction
-data class DirOutput(val name: String): Instruction
-data class FileOutput(val name: String, val size: Int): Instruction
 
-
-fun String.toInstruction() = when {
-    startsWith("$ cd ") -> {
-        val loc = substringAfter("$ cd ")
-        if (loc == "..") {
-            Up
-        } else
-            Cd(loc)
+fun List<String>.computeFolderContent(parent: Node): List<Node> = drop(1).map {
+    val (a, b) = it.split(" ")
+    when (a) {
+        "dir" -> Directory(b, parent)
+        else -> File(a.toInt(), b, parent)
     }
-    this == "$ ls" -> Ls
-    startsWith("dir ") -> DirOutput(substringAfter("dir "))
-    this[0].isDigit() -> {
-        val (a, b) = split(" ")
-        FileOutput(b, a.toInt())
-    }
-    else -> error("Can't parse: $this")
 }
-fun List<String>.toInstructions() = map {it.toInstruction() }
 
-fun List<Instruction>.buildFileTree(): Directory {
-    val root = Directory(null)
-    var current = root
-    forEach {
-        current = when (it) {
-            is Up -> current.parent!!
-            Cd("/"), is Ls -> current
-            is Cd -> current[it.dir]!!
-            is DirOutput -> current.apply {
-                this[it.name] = Directory(current)
+fun List<String>.parseInput(): Directory {
+    val realInput = drop(1).joinToString("\n").split("$ ")
+    var cur = Directory("/", null)
+    val root = cur
+    for (i in realInput.drop(1).map { it.lines().filter { l -> l.isNotBlank() } }) {
+        when {
+            i.first().startsWith("ls") -> {
+                val children = i.computeFolderContent(cur)
+                cur.children.addAll(children)
             }
-            is FileOutput -> {
-                current.totalSize += it.size
-                current
+            else -> {
+                val (_, dir) = i.first().split(" ")
+                cur = when (dir) {
+                    ".." -> cur.parent as Directory
+                    "/" -> root
+                    else -> cur.children.find { it.name == dir } as? Directory ?: error(dir)
+                }
             }
         }
     }
-    root.updateTotalSizes()
     return root
 }
 
-class Directory(
-    val parent: Directory?,
-    private val childDirs: MutableMap<String, Directory> = mutableMapOf()
-): MutableMap<String, Directory> by childDirs {
-    var totalSize: Int = 0
-
-    fun find(predicate: (Directory) -> Boolean): List<Directory> =
-        if (predicate(this)) {
-            listOf(this)
-        } else {
-            emptyList()
-        } +
-        flatMap { (_, n) -> n.find(predicate) }
-
-    fun updateTotalSizes(): Int {
-        totalSize += childDirs.values.sumOf { it.updateTotalSizes() }
-        return totalSize
-    }
-}
-
 class Day07 : AdventDay<Int>(2022, 7) {
-    override fun part1(input: List<String>): Int = input
-        .toInstructions()
-        .also { it.forEach { l -> println(l) } }
-        .buildFileTree()
-        .find { it.totalSize <= 100000 }
-        .sumOf { it.totalSize }
+    override fun part1(input: List<String>): Int {
+        val root = input.parseInput()
+        return root.filterInTree { it.fileSize <= 100_000 }.sumOf { it.fileSize }
+    }
     override fun part2(input: List<String>): Int {
-        val root = input.toInstructions().buildFileTree()
-        val excess = 30000000 - (70000000 - root.totalSize)
-        return root.find { it.totalSize >= excess }
-            .minBy { it.totalSize }
-            .totalSize
+        val root = input.parseInput()
+        val currentSize = root.fileSize
+        return root.filterInTree {
+            70_000_000 - (currentSize - it.fileSize)  >= 30_000_000
+        }.minOf { it.fileSize }
     }
 }
 
