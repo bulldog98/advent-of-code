d3a56951383ff66856a6937bc216b6b21c068fab
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-09 07:27:51 +0100

    Prepare for Day10

---
+++ advent2022/src/main/kotlin/Day10.kt
@@ -0,0 +1,7 @@
+class Day10 : AdventDay<Int>(2022, 10) {
+    override fun part1(input: List<String>): Int = 0
+
+    override fun part2(input: List<String>): Int = 0
+}
+
+fun main() = Day10().run()
