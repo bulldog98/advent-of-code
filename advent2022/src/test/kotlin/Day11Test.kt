a8fb8db790ff51ede8fe122605c0086ab9094db2
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-11 08:09:56 +0100

    Day11: Initial solution

--- advent2022/src/test/kotlin/Day11Test.kt
+++ advent2022/src/test/kotlin/Day11Test.kt
@@ -5,9 +5,12 @@ class Day11Test {
     private val day = Day11()
 
     @Test
-    fun part1() = assertEquals(0, day.testPart1())
+    fun part1() = assertEquals(10605L, day.testPart1())
 
 
     @Test
-    fun part2() = assertEquals(0, day.testPart2())
+    fun part2() = assertEquals(
+        2713310158,
+        day.testPart2()
+    )
 }
