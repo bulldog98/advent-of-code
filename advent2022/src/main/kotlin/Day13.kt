829c821b3afb389d6bce97a019a8fcc97ff918c0
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-12 20:13:05 +0100

    Day13: prepare

---
+++ advent2022/src/main/kotlin/Day13.kt
@@ -0,0 +1,9 @@
+class Day13 : AdventDay(2022, 13) {
+    override fun part1(input: List<String>): Int =
+        TODO()
+
+    override fun part2(input: List<String>): Int =
+        TODO()
+}
+
+fun main() = Day13().run()
