caa256a91b6d6e1e56e907f4aee4c68020297379
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-21 22:11:04 +0100

    Day22: prepare for day

---
+++ advent2022/src/main/kotlin/Day22.kt
@@ -0,0 +1,8 @@
+class Day22 : AdventDay(2022, 22) {
+    override fun part1(input: List<String>): Int =
+        TODO("Not yet implemented")
+    override fun part2(input: List<String>): Int =
+        TODO("Not yet implemented")
+}
+
+fun main() = Day22().run()
---
+++ advent2022/src/test/kotlin/Day22Test.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day22Test {
+    private val day = Day22()
+
+    @Test
+    fun part1() = assertEquals(1, day.testPart1())
+
+
+    @Test
+    fun part2() = assertEquals(1, day.testPart2())
+}
