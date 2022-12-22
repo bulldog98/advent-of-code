e2b75962dfe44977d2af7b556737a05c7f64f18d
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2022-12-22 20:38:21 +0100

    Day23: prepare

---
+++ advent2022/src/main/kotlin/Day23.kt
@@ -0,0 +1,8 @@
+class Day23 : AdventDay(2022, 23) {
+    override fun part1(input: List<String>): Int =
+        TODO("Not yet implemented")
+    override fun part2(input: List<String>): Int =
+        TODO("Not yet implemented")
+}
+
+fun main() = Day23().run()
