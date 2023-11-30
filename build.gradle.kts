4983063b435464f626f86103db56e403bbbe5f02
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-11-30 23:18:17 +0100

    centralize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val coroutinesVersion by extra { "1.6.4" }
 plugins {
     kotlin("jvm") version "1.7.22"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.7.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.22"
     id("org.jetbrains.kotlinx.benchmark") version "0.4.4"
 }
 
@@ -19,9 +20,42 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
     apply(plugin = "org.jetbrains.kotlinx.benchmark")
 
+    tasks.getByName<Test>("test") {
+        useJUnitPlatform()
+    }
+
+    if (name != "common") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.serialization" )
+
+        val copyForTest = task<Copy>("copyDataForTests") {
+            from("$rootDir/data/**.txt")
+            into("$projectDir/data")
+        }
+
+        tasks.getByName<Test>("test") {
+            dependsOn.add(copyForTest)
+        }
+
+        sourceSets {
+            main {
+                resources {
+                    includes += "**.txt"
+                }
+            }
+        }
+    }
+
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.22")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+        if (name != "common") {
+            implementation(project(":common"))
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+        }
+
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     }
 
     allOpen {
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,11 +4,4 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
     implementation("io.ktor:ktor-client-encoding:$ktorVersion")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
