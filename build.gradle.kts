0b451b156afe9235ef526976f059b60615d7d883
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-11-30 23:28:09 +0100

    chore(deps): update kotlin to 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ val ktorVersion by extra { "2.1.3" }
 val coroutinesVersion by extra { "1.6.4" }
 
 plugins {
-    kotlin("jvm") version "1.7.22"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.7.22"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
     id("org.jetbrains.kotlinx.benchmark") version "0.4.4"
 }
 
@@ -46,7 +46,7 @@ subprojects {
     }
 
     dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.22")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
         if (name != "common") {
