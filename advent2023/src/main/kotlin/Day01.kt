d65075b553a4169db74a7b386af38af9a3262326
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-11-30 23:54:58 +0100

    chore: prepare 2023 Day 1

---
+++ advent2023/src/main/kotlin/Day01.kt
@@ -0,0 +1,9 @@
+class Day01 : AdventDay(2023, 1) {
+    override fun part1(input: List<String>): Int =
+        TODO("Not yet implemented")
+
+    override fun part2(input: List<String>): Int =
+        TODO("Not yet implemented")
+}
+
+fun main() = Day01().run()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "advent-of-code"
 include("common")
 include("advent2022")
+include("advent2023")
