08c6ded7e1a8d3ccab036e66362a5b1529b6dc08
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-01 17:11:11 +0100

    chore: make advent2021 a module

--- advent2021/build.gradle.kts
+++ advent2021/build.gradle.kts
@@ -1,19 +1,3 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-}
-
-repositories {
-    mavenCentral()
-}
-
-tasks {
-    sourceSets {
-        main {
-            java.srcDirs("src")
-        }
-    }
-
-    wrapper {
-        gradleVersion = "7.3"
-    }
+    id("de.bulldog98.plugin.adventOfCode")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "advent-of-code"
 include("common")
+include("advent2021")
 include("advent2022")
 include("advent2023")
