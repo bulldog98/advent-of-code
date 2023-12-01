76dc1bb381759588fdcd10c35cdec19486b2f8f9
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-01 17:57:50 +0100

    refactor(2021): Day07 to use AdventDay

---
+++ advent2021/src/main/kotlin/Day07.kt
@@ -0,0 +1,33 @@
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+private fun addIfFirstMaxIsZero(a: Int, b: Int): Int =
+    if (a == Int.MAX_VALUE)
+        b
+    else
+        a + b
+
+private fun Collection<Int>.alignTo(i: Int, cost: (Int) -> Int = ::abs): Int = asSequence()
+    .map { max(it, i) - min(it, i) }
+    .filter { cost(it) > 0 }
+    .map { cost(it) }
+    .fold(Int.MAX_VALUE, ::addIfFirstMaxIsZero)
+
+fun increaseByStep(n: Int): Int = (n * (n+1)) / 2
+
+object Day07 : AdventDay(2021, 7) {
+    override fun part1(input: List<String>): Int = with(input[0].split(",").map(String::toInt)) {
+        indices.minOfOrNull {
+            this.alignTo(it)
+        } ?: throw Error("no valid config")
+    }
+
+    override fun part2(input: List<String>): Int = with(input[0].split(",").map(String::toInt)) {
+        indices.minOfOrNull {
+            this.alignTo(it, ::increaseByStep)
+        } ?: throw Error("no valid config")
+    }
+}
+
+fun main() = Day07.run()
---
+++ advent2021/src/test/kotlin/Day07Test.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day07Test {
+    @Test
+    fun part1() = Assertions.assertEquals(37, Day07.testPart1())
+
+    @Test
+    fun part2() = Assertions.assertEquals(168, Day07.testPart2())
+}
