96fe8ad668868f51fc562fb18bdb39f3f6aa7a04
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-01 18:04:04 +0100

    refactor(2021): Day08 to use AdventDay

---
+++ advent2021/src/test/kotlin/Day08Test.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+    @Test
+    fun part1() = Assertions.assertEquals(26, Day08.testPart1())
+
+    @Test
+    fun part2() = Assertions.assertEquals(61229, Day08.testPart2())
+}
