abebae7ecc549750ce760433717810f2a66ba3e3
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-01 20:43:04 +0100

    feat(2021): solve day 10

---
+++ advent2021/src/main/kotlin/Day10.kt
@@ -0,0 +1,21 @@
+import day10.computeEndState
+import day10.scoreCompletion
+
+object Day10 : AdventDay(2021, 10) {
+    override fun part1(input: List<String>): Int =
+        input.sumOf { line ->
+            line.computeEndState().first?.errorScore ?: 0
+        }
+
+    override fun part2(input: List<String>): Long =
+        input
+            .map { it.computeEndState() }
+            .filter { it.first == null }
+            .map { it.second.reversed().scoreCompletion() }
+            .sorted()
+            .let {
+                it[it.size / 2]
+            }
+}
+
+fun main() = Day10.run()
