13d41cc1914038e65f9fe240ffc872861b4e99cb
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-03 06:45:47 +0100

    feat(2023): solve day 3 part 2

--- advent2023/src/main/kotlin/year2023/Day03.kt
+++ advent2023/src/main/kotlin/year2023/Day03.kt
@@ -9,17 +9,7 @@ data class DataSymbol(val symbol: Char): Data
 
 object Day03 : AdventDay(2023, 3) {
     override fun part1(input: List<String>): Any {
-        val board = input.asSequence().withIndex().flatMap { (y, line) ->
-            val digits = "(\\d)+".toRegex().findAll(line).flatMap {
-                val positions = it.range.map { x -> Point2D(x = x, y = y) }
-                val number = DataNumber(it.value.toInt(), positions)
-                positions.map { pos -> pos to number }
-            }
-            val chars = line.withIndex()
-                .filter { !it.value.isDigit() && it.value != '.' }
-                .map { (x, c) -> Point2D(x = x, y = y) to DataSymbol(c) }
-            digits + chars
-        }.toMap()
+        val board = parseInput(input)
         val nums = board.entries
             .filter {
                 it.value is DataSymbol
@@ -32,8 +22,32 @@ object Day03 : AdventDay(2023, 3) {
         return nums.sumOf { it.num }
     }
 
+    private fun parseInput(input: List<String>) = input.asSequence().withIndex().flatMap { (y, line) ->
+        val digits = "(\\d)+".toRegex().findAll(line).flatMap {
+            val positions = it.range.map { x -> Point2D(x = x, y = y) }
+            val number = DataNumber(it.value.toInt(), positions)
+            positions.map { pos -> pos to number }
+        }
+        val chars = line.withIndex()
+            .filter { !it.value.isDigit() && it.value != '.' }
+            .map { (x, c) -> Point2D(x = x, y = y) to DataSymbol(c) }
+        digits + chars
+    }.toMap()
+
     override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
+        val board = parseInput(input)
+        return board.entries
+            .map {
+                it.value to it.key.neighborHood
+                    .mapNotNull { pos -> board[pos] }
+                    .filterIsInstance<DataNumber>()
+                    .toSet()
+            }.filter { (value, surroundingNumbers) ->
+                value is DataSymbol && value.symbol == '*' && surroundingNumbers.size == 2
+            }.sumOf { (_, surroundings) ->
+                val (num1, num2) = surroundings.toList()
+                num1.num * num2.num
+            }
     }
 }
 
--- advent2023/src/test/kotlin/year2023/Day03Test.kt
+++ advent2023/src/test/kotlin/year2023/Day03Test.kt
@@ -6,4 +6,6 @@ import org.junit.jupiter.api.Test
 class Day03Test {
     @Test
     fun part1() = Assertions.assertEquals(4361, Day03.testPart1())
+    @Test
+    fun part2() = Assertions.assertEquals(467835, Day03.testPart2())
 }
