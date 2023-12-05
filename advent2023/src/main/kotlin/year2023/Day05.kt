7ebd4a8415e278d40c28e953368ce2611bc1bc89
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-05 07:10:54 +0100

    feat(2023): solve day 5 part 2

--- advent2023/src/main/kotlin/year2023/Day05.kt
+++ advent2023/src/main/kotlin/year2023/Day05.kt
@@ -6,34 +6,52 @@ import helper.numbers.NUMBERS_REGEX
 data class Mapping(
     val from: String,
     val to: String,
+    val range: LongRange,
     val mapping: (Long) -> Long?
 ): (Long) -> Long? by mapping {
     companion object {
-        private fun helper(line: String): (Long) -> Long? {
-            val (to, start, size) = NUMBERS_REGEX.findAll(line).map { it.value.toLong() }.toList()
-            val range = (start until start + size)
+        private fun LongRange.toMapFunction(to: Long): (Long) -> Long? {
             return {
-                if (it in range) {
-                    it - start + to
+                if (it in this) {
+                    it - this.first + to
                 } else {
                     null
                 }
             }
         }
 
+        private fun helper(line: String): LongRange {
+            val (_, start, size) = NUMBERS_REGEX.findAll(line).map { it.value.toLong() }.toList()
+            return (start until start + size)
+        }
+
         fun of(from: String, to: String, line: String) = Mapping(
             from,
             to,
-            helper(line)
+            helper(line),
+            helper(line).toMapFunction(NUMBERS_REGEX.find(line)?.value?.toLong()!!)
         )
     }
 }
 
-object Day05 : AdventDay(2023, 5) {
-    override fun part1(input: List<String>): Any {
-        val seeds = NUMBERS_REGEX.findAll(input[0]).map { it.value.toLong() }.toList()
+fun LongRange.splitBy(mappings: List<Mapping>): List<LongRange> = when {
+    isEmpty() -> emptyList()
+    mappings.none { first in it.range || last in it.range } -> listOf(this)
+    else -> {
+        val firstMapping = mappings.firstOrNull { first in it.range }
+        if (firstMapping != null) {
+            val newEnd = last { it in firstMapping.range }
+            listOf(first..newEnd) + (newEnd + 1..last).splitBy(mappings)
+        } else {
+            val startOfRange = first { mappings.any { m -> it in m.range } }
+            listOf((first until  startOfRange)) + (startOfRange..last).splitBy(mappings)
+        }
+    }
+}
 
-        val inputs = input.joinToString("\n")
+object Day05 : AdventDay(2023, 5) {
+    private fun List<String>.computeMappings() =
+        joinToString("\n")
             .split("\n\n")
             .drop(1)
             .map(String::lines)
@@ -43,9 +61,14 @@ object Day05 : AdventDay(2023, 5) {
                     Mapping.of(from, to, it)
                 }
             }
-        val res = inputs.fold(seeds) { last, mappings ->
+
+    override fun part1(input: List<String>): Any {
+        val seeds = NUMBERS_REGEX.findAll(input[0]).map { it.value.toLong() }.toList()
+
+        val mappings = input.computeMappings()
+        val res = mappings.fold(seeds) { last, mapping ->
             last.map { lastTypeNumber ->
-                mappings.firstNotNullOfOrNull { mapping ->
+                mapping.firstNotNullOfOrNull { mapping ->
                     mapping(lastTypeNumber)
                 } ?: lastTypeNumber
             }
@@ -54,7 +77,28 @@ object Day05 : AdventDay(2023, 5) {
     }
 
     override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
+        val seedRanges = NUMBERS_REGEX.findAll(input[0])
+            .map { it.value.toLong() }
+            .chunked(2)
+            .map { (start, size) -> (start until start + size) }
+            .toList()
+
+        val mappings = input.computeMappings()
+
+        val res = mappings.fold(seedRanges) { last, mapping ->
+            last.flatMap { range ->
+                    range.splitBy(mapping)
+                }.map { range ->
+                    mapping
+                        .firstOrNull { range.first in it.range }
+                        ?.let {
+                            val firstMapped = it(range.first) ?: error("could not map ${range.first} with $it")
+                            val lastMapped = it(range.last) ?: error("could not map ${range.last} with $it")
+                            firstMapped..lastMapped
+                        } ?: range
+                }
+        }
+        return res.minOf { it.first }
     }
 }
 
--- advent2023/src/test/kotlin/year2023/Day05Test.kt
+++ advent2023/src/test/kotlin/year2023/Day05Test.kt
@@ -6,4 +6,17 @@ import org.junit.jupiter.api.Test
 class Day05Test {
     @Test
     fun part1() = Assertions.assertEquals(35L, Day05.testPart1())
+
+    @Test
+    fun stupidCase() {
+        val range = 57L..69L
+        val mapping = listOf(Mapping("fertilizer", "stuff", 53L..60L) { it })
+
+        val res = range.splitBy(mapping)
+
+        Assertions.assertEquals(2, res.size)
+    }
+
+    @Test
+    fun part2() = Assertions.assertEquals(46L, Day05.testPart2())
 }
