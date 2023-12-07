469ff4dc32366724b8060eafb98bee508f0da633
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-07 20:11:45 +0100

    feat(2023): solve day 7 part 2

--- advent2023/src/main/kotlin/year2023/Day07.kt
+++ advent2023/src/main/kotlin/year2023/Day07.kt
@@ -2,7 +2,7 @@ package year2023
 
 import AdventDay
 
-fun Char.cardTypeValue() = when (this) {
+fun Char.cardTypeValue(jokerBad: Boolean = false) = when (this) {
     '2' -> 'b'
     '3' -> 'c'
     '4' -> 'd'
@@ -12,29 +12,88 @@ fun Char.cardTypeValue() = when (this) {
     '8' -> 'h'
     '9' -> 'i'
     'T' -> 'j'
-    'J' -> 'k'
+    'J' -> if (jokerBad) 'a' else 'k'
     'Q' -> 'l'
     'K' -> 'm'
     'A' -> 'n'
     else -> error("can not happen")
 }
 
-data class Hand(val cards: String, val bid: Int) {
-    val type: Int
-        get() {
-            val counts = cards.groupingBy { it }.eachCount().values
+fun String.asCardTypeValues(jokerBad: Boolean = false) =
+    map { c -> c.cardTypeValue(jokerBad) }.joinToString("")
+
+sealed interface HandType: Comparable<HandType> {
+    val orderNumber: Int
+    override operator fun compareTo(other: HandType): Int =
+        compareBy<HandType> { it.orderNumber }.compare(this, other)
+    data object FiveOfAKind: HandType {
+        override val orderNumber: Int = 6
+    }
+    data object FourOfAKind: HandType {
+        override val orderNumber: Int = 5
+    }
+    data object FullHouse: HandType {
+        override val orderNumber: Int = 4
+    }
+    data object ThreeOfAKind: HandType {
+        override val orderNumber: Int = 3
+    }
+    data object TwoPair: HandType {
+        override val orderNumber: Int = 2
+    }
+    data object OnePair: HandType {
+        override val orderNumber: Int = 1
+    }
+    data object HighCard: HandType {
+        override val orderNumber: Int = 0
+    }
+}
+
+data class Hand(val cards: String, val bid: Int, val type: HandType) {
+    companion object {
+        private fun String.computeTypeWithJoker(): HandType {
+            val jokerCount = count { it == 'J' }
+            val counts = filter { it != 'J' }.groupingBy { it }.eachCount().values
             return when {
-                5 in counts -> 6
-                4 in counts -> 5
-                3 in counts && 2 in counts -> 4
-                3 in counts -> 3
-                counts.count { it == 2 } == 2 -> 2
-                2 in counts -> 1
-                else -> 0
+                jokerCount == 0 -> computeType()
+                jokerCount >= 4 -> HandType.FiveOfAKind
+                jokerCount == 3 && 2 in counts ->  HandType.FiveOfAKind
+                jokerCount == 3 -> HandType.FourOfAKind
+                jokerCount == 2 -> when {
+                    3 in counts -> HandType.FiveOfAKind
+                    2 in counts -> HandType.FourOfAKind
+                    else -> HandType.ThreeOfAKind
+                }
+                jokerCount == 1 -> when {
+                    4 in counts -> HandType.FiveOfAKind
+                    3 in counts -> HandType.FourOfAKind
+                    counts.count { it == 2 } == 2 -> HandType.FullHouse
+                    2 in counts -> HandType.ThreeOfAKind
+                    else -> HandType.OnePair
+                }
+                else -> HandType.HighCard
             }
         }
-    companion object {
-        operator fun invoke(input: List<String>) = Hand(input[0], input[1].toInt())
+        private fun String.computeType(): HandType {
+            val counts = groupingBy { it }.eachCount().values
+            return when {
+                5 in counts -> HandType.FiveOfAKind
+                4 in counts -> HandType.FourOfAKind
+                3 in counts && 2 in counts -> HandType.FullHouse
+                3 in counts -> HandType.ThreeOfAKind
+                counts.count { it == 2 } == 2 -> HandType.TwoPair
+                2 in counts -> HandType.OnePair
+                else -> HandType.HighCard
+            }
+        }
+        operator fun invoke(input: List<String>, jokersViewed: Boolean = false) = Hand(
+            input[0],
+            input[1].toInt(),
+            if (jokersViewed)
+                input[0].computeTypeWithJoker()
+            else
+                input[0].computeType()
+        )
     }
 }
 
@@ -45,13 +104,20 @@ object Day07 : AdventDay(2023, 7) {
             .map {
                 Hand(it.split(" "))
             }
-            .sortedWith(compareBy({ it.type }, { it.cards.map { c -> c.cardTypeValue() }.joinToString("") }))
+            .sortedWith(compareBy({ it.type }, { it.cards.asCardTypeValues() }))
             .mapIndexed { i, it -> it to i + 1 }
             .sumOf { it.first.bid * it.second }
 
-    override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
-    }
+    override fun part2(input: List<String>): Any =
+        input
+            .asSequence()
+            .map {
+                Hand(it.split(" "), true)
+            }
+            .sortedWith(compareBy({ it.type }, { it.cards.asCardTypeValues(true) }))
+            .mapIndexed { i, it -> it to i + 1 }
+            .also { it.forEach(::println) }
+            .sumOf { it.first.bid * it.second }
 }
 
 fun main() = Day07.run()
--- advent2023/src/test/kotlin/year2023/Day07Test.kt
+++ advent2023/src/test/kotlin/year2023/Day07Test.kt
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day07Test {
-
     @Test
     fun part1() = Assertions.assertEquals(6440, Day07.testPart1())
+
+    @Test
+    fun part2() = Assertions.assertEquals(5905, Day07.testPart2())
 }
