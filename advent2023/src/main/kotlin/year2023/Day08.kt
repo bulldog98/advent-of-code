a53ce133f1fc91e3d29c866ad5cd6177b32e9967
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-08 07:03:48 +0100

    feat(2023): solve day 8 part 2

--- advent2023/src/main/kotlin/year2023/Day08.kt
+++ advent2023/src/main/kotlin/year2023/Day08.kt
@@ -1,31 +1,53 @@
 package year2023
 
 import AdventDay
+import lcm
 
-object Day08: AdventDay(2023, 8) {
+fun computeNumberOfSteps(
+    node: String,
+    instructions: String,
+    ways: Map<String, Pair<String, String>>,
+    endCondition: (String) -> Boolean
+): Long {
+    var currentNode = node
+    var i = 0L
+    while (!endCondition(currentNode)) {
+        val (l, r) = ways[currentNode]!!
+        val inst = instructions[(i % instructions.length).toInt()]
+        i++
+        currentNode = when (inst) {
+            'R' -> r
+            'L' -> l
+            else -> error("should not happen")
+        }
+    }
+    return i
+}
+
+object Day08 : AdventDay(2023, 8) {
     override fun part1(input: List<String>): Any {
         val instructions = input[0]
         val ways = input.drop(2).associate { line ->
             val (node, left, right) = line.split("=", "(", ")", ",").filter { it.isNotBlank() }.map { it.trim() }
             node to (left to right)
         }
-        var i = 0
-        var node = "AAA"
-        while (node != "ZZZ") {
-            val (l, r) = ways[node]!!
-            val inst = instructions[i % instructions.length]
-            i++
-            node = when (inst) {
-                'R' -> r
-                'L' -> l
-                else -> error("should not happen")
-            }
-        }
-        return i
+        return computeNumberOfSteps("AAA", instructions, ways) { node -> node == "ZZZ" }
     }
 
+    // works since all nodes ending with Z can go instruction.length to get to the node again
     override fun part2(input: List<String>): Any {
-        TODO()
+        val instructions = input[0]
+        val ways = input.drop(2).associate { line ->
+            val (node, left, right) = line.split("=", "(", ")", ",").filter { it.isNotBlank() }.map { it.trim() }
+            node to (left to right)
+        }
+        val counts = ways.keys.filter { it.last() == 'A' }
+            .map { node ->
+                computeNumberOfSteps(node, instructions, ways) {
+                    it.last() == 'Z'
+                }
+            }
+        return lcm(counts)
     }
 }
 
--- advent2023/src/test/kotlin/year2023/Day08Test.kt
+++ advent2023/src/test/kotlin/year2023/Day08Test.kt
@@ -5,5 +5,7 @@ import org.junit.jupiter.api.Test
 
 class Day08Test {
     @Test
-    fun part1() = Assertions.assertEquals(6, Day08.testPart1())
+    fun part1() = Assertions.assertEquals(6L, Day08.testPart1())
+    @Test
+    fun part2() = Assertions.assertEquals(6L, Day08.testPart2())
 }
