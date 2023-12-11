0e6837906033e824e1d350db4d583ea99fce5766
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-11 07:03:52 +0100

    refactor(2023): day 11

--- advent2023/src/main/kotlin/year2023/Day11.kt
+++ advent2023/src/main/kotlin/year2023/Day11.kt
@@ -6,61 +6,49 @@ import findAllPositionsOf
 import kotlin.math.abs
 
 object Day11 : AdventDay(2023, 11) {
-    private fun Collection<Int>.computeOffsetFor(index: Int): Int =
-        this.count { it < index } + index
+    private fun Point2D.manhattanDistance(other: Point2D): Long =
+        abs(x - other.x) + abs(y - other.y)
 
-    private fun Point2D.manhattenDistance(other: Point2D): Long = abs(x - other.x) + abs(y - other.y)
-    override fun part1(input: List<String>): Any {
-        val blankColumns = input[0].indices.filter { input.all { line -> line[it] == '.' } }.toSet()
-        val skyMapExpanded = input
-            .flatMap { line ->
-                if (line.all { c -> c == '.' })
-                    listOf(line, line)
-                else
-                    listOf(line)
-            }
-            .flatMapIndexed { y, line ->
-                line.flatMapIndexed { x, char ->
-                    val pointItself = Point2D(blankColumns.computeOffsetFor(x), y) to char
-                    if (x in blankColumns) {
-                        listOf(pointItself, pointItself.first + Point2D.RIGHT to '.')
-                    } else {
-                        listOf(pointItself)
-                    }
-                }
-            }.associate { it }
-        val galaxies = skyMapExpanded.filterValues { it == '#' }.keys.toList()
-        val pairs = buildList {
-            galaxies.indices.forEach { x ->
-                (x..galaxies.indices.last).forEach { y ->
+    private fun manhattanDistance(point2DPoint2DPair: Pair<Point2D, Point2D>) =
+        point2DPoint2DPair.first.manhattanDistance(point2DPoint2DPair.second)
+
+    private fun List<Point2D>.allPairs() =
+        buildList {
+            this@allPairs.indices.forEach { x ->
+                (x..this@allPairs.indices.last).forEach { y ->
                     if (x != y) {
-                        this += galaxies[x] to galaxies[y]
+                        this += this@allPairs[x] to this@allPairs[y]
                     }
                 }
             }
         }
-        return pairs.sumOf { (a, b) -> a.manhattenDistance(b) }
+
+    private fun List<String>.blankColumIndices() =
+        this[0].indices.filter { all { line -> line[it] == '.' } }.toSet()
+    private fun List<String>.blankRowIndices() =
+        indices.filter { this[it].all { c -> c == '.' } }.toSet()
+
+    private fun List<String>.computeGalaxiesWithWithAdjustment(expansionForEmpty: Long = 2): List<Point2D> {
+        val blankRows = blankRowIndices()
+        val blankColumns = blankColumIndices()
+        val unadjustedGalaxies = findAllPositionsOf()
+        return unadjustedGalaxies.map {
+            val newX = it.x + blankColumns.count { c -> c < it.x }.toLong() * (expansionForEmpty - 1L)
+            val newY = it.y + blankRows.count { c -> c < it.y }.toLong() * (expansionForEmpty - 1L)
+            Point2D(newX, newY)
+        }
+    }
+
+    override fun part1(input: List<String>): Any {
+        val galaxies = input.computeGalaxiesWithWithAdjustment(2)
+        val pairs = galaxies.allPairs()
+        return pairs.sumOf(::manhattanDistance)
     }
 
     override fun part2(input: List<String>): Any {
-        val blankRows = input.indices.filter { input[it].all { c -> c == '.' } }.toSet()
-        val blankColumns = input[0].indices.filter { input.all { line -> line[it] == '.' } }.toSet()
-        val unadjustedGalaxies = input.findAllPositionsOf()
-        val galaxies = unadjustedGalaxies.map {
-            val newX = it.x + blankColumns.count { c -> c < it.x }.toLong() * (1_000_000 - 1).toLong()
-            val newY = it.y + blankRows.count { c -> c < it.y }.toLong() * (1_000_000 - 1).toLong()
-            Point2D(newX, newY)
-        }
-        val pairs = buildList {
-            galaxies.indices.forEach { x ->
-                (x..galaxies.indices.last).forEach { y ->
-                    if (x != y) {
-                        this += galaxies[x] to galaxies[y]
-                    }
-                }
-            }
-        }
-        return pairs.sumOf { (a, b) -> a.manhattenDistance(b) }
+        val galaxies = input.computeGalaxiesWithWithAdjustment(1_000_000L)
+        val pairs = galaxies.allPairs()
+        return pairs.sumOf(::manhattanDistance)
     }
 }
 
