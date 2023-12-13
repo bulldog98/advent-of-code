c20b404904e944f4f0fb08ead443ae7845133d55
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-13 12:29:51 +0100

    feat(2023): solve day 13 part 2

--- advent2023/src/main/kotlin/year2023/Day13.kt
+++ advent2023/src/main/kotlin/year2023/Day13.kt
@@ -1,6 +1,7 @@
 package year2023
 
 import AdventDay
+import Point2D
 
 object Day13: AdventDay(2023, 13) {
     sealed interface Reflection {
@@ -15,9 +16,9 @@ object Day13: AdventDay(2023, 13) {
             get() = column1
     }
 
-    private fun List<String>.findHorizontalReflection(): HorizontalReflection? {
+    private fun List<String>.findHorizontalReflection(): List<HorizontalReflection> {
         val potentialRows = indices.filter { it + 1 in indices && this[it] == this[it + 1] }
-        return potentialRows.firstOrNull {
+        return potentialRows.filter {
             (0..(size / 2)).all {  delta ->
                 if (it + 1 + delta in indices && it - delta in indices) {
                     this[it - delta] == this[it + 1 + delta]
@@ -25,15 +26,15 @@ object Day13: AdventDay(2023, 13) {
                     true
                 }
             }
-        }?.let { x -> HorizontalReflection(x + 1, x + 2) }
+        }.map { x -> HorizontalReflection(x + 1, x + 2) }
     }
 
     private fun List<String>.columnsEqual(c1: Int, c2: Int) =
         all { it[c1] == it[c2] }
 
-    private fun List<String>.findVerticalReflection(): VerticalReflection? {
+    private fun List<String>.findVerticalReflection(): List<VerticalReflection> {
         val potentialColumns = this[0].indices.filter { x -> x + 1 in this[0].indices && this.all { it[x] == it[x + 1] } }
-        return potentialColumns.firstOrNull {
+        return potentialColumns.filter {
             (0..(this[0].length / 2)).all {  delta ->
                 if (it + 1 + delta in this[0].indices && it - delta in this[0].indices) {
                     this.columnsEqual(it - delta, it + 1 + delta)
@@ -41,22 +42,57 @@ object Day13: AdventDay(2023, 13) {
                     true
                 }
             }
-        }?.let { x -> VerticalReflection(x + 1, x + 2) }
+        }.map { x -> VerticalReflection(x + 1, x + 2) }
     }
 
-    private fun List<String>.findReflection(): Reflection =
-        findHorizontalReflection() ?: findVerticalReflection() ?: error("could not find reflection")
+    private fun List<String>.findReflections(): List<Reflection> =
+        findHorizontalReflection() + findVerticalReflection()
 
-    override fun part1(input: List<String>): Any {
+    private fun List<String>.replaceSmudgeAt(point: Point2D): List<String> = mapIndexed { y, line ->
+        if (point.y == y.toLong()) {
+            line.mapIndexed { x, c ->
+                when (point.x) {
+                    x.toLong()-> if (c == '.') '#' else '.'
+                    else -> c
+                }
+            }.joinToString("")
+        } else
+            line
+    }
+
+    private fun List<String>.reflectionAfterReplacingSmudge(): List<Reflection> = generateSequence(0 to 0) { (x, y) ->
+        when (this[0].length) {
+            x + 1 -> {
+                if (y + 1 == size) {
+                    null
+                } else {
+                    0 to y + 1
+                }
+            }
+            else -> x + 1 to y
+        }
+    }.map {
+        replaceSmudgeAt(Point2D(it.first, it.second))
+    }.map {
+        it.findReflections()
+    }.firstOrNull {
+        it.size > 1 || (it.isNotEmpty() && findReflections() != it)
+    } ?: findReflections()
+
+    override fun part1(input: List<String>): Long {
         val parsed = input.joinToString("\n").split("\n\n").asSequence().map { it.lines() }
         return parsed.sumOf {
-            val reflection = it.findReflection()
-            reflection.value.toLong()
+            val reflection = it.findReflections()
+            reflection.single().value.toLong()
         }
     }
 
-    override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
+    override fun part2(input: List<String>): Long {
+        val parsed = input.joinToString("\n").split("\n\n").asSequence().map { it.lines() }
+        return parsed.sumOf {
+            val reflection = it.reflectionAfterReplacingSmudge()
+            (reflection - it.findReflections().toSet()).single().value.toLong()
+        }
     }
 }
 
--- advent2023/src/test/kotlin/year2023/Day13Test.kt
+++ advent2023/src/test/kotlin/year2023/Day13Test.kt
@@ -6,4 +6,6 @@ import org.junit.jupiter.api.Test
 class Day13Test {
     @Test
     fun part1() = Assertions.assertEquals(405L, Day13.testPart1())
+    @Test
+    fun part2() = Assertions.assertEquals(400L, Day13.testPart2())
 }
