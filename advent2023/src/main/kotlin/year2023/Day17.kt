bd542250ab0b7c694d859028fa15af3022c55fd7
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-17 09:33:33 +0100

    feat(2023): day 17 part 2
    the solution for part 2 takes ~1 h to compute

--- advent2023/src/main/kotlin/year2023/Day17.kt
+++ advent2023/src/main/kotlin/year2023/Day17.kt
@@ -3,59 +3,73 @@ package year2023
 import AdventDay
 import Point2D
 import graph.AdjacencyListGraph
+import graph.Graph
 import graph.dijkstra
 
 object Day17 : AdventDay(2023, 17) {
     data class CityBlocks(val input: List<String>) {
-        val nodes = input.indices.flatMap { y ->
+        private val nodes = input.indices.flatMap { y ->
             input[y].indices.map { x -> Point2D(x, y) }
         }.toSet()
+
+        fun buildGraph(min: Int, max: Int): Graph<CrucibleTmpPosition> {
+            val realNodes = this.nodes
+                .flatMap {
+                    listOf(Point2D.LEFT, Point2D.RIGHT, Point2D.UP, Point2D.DOWN).flatMap { direction ->
+                        (1 ..max).map { tilesMoved ->
+                            CrucibleTmpPosition(it, direction, tilesMoved)
+                        }
+                    }.filter { it.position in nodes }
+                }.toSet()
+            return AdjacencyListGraph(realNodes) { u ->
+                val leftRight = when (u.direction) {
+                    Point2D.UP, Point2D.DOWN -> listOf(Point2D.LEFT, Point2D.RIGHT)
+                    Point2D.LEFT, Point2D.RIGHT -> listOf(Point2D.UP, Point2D.DOWN)
+                    else -> error("not a valid direction ${u.direction}")
+                }
+                val turnAround = leftRight
+                    .map { CrucibleTmpPosition(u.position + it, it) }
+                when {
+                    u.tilesMovedInDirection == max -> turnAround
+                    u.tilesMovedInDirection < min ->
+                        listOf(
+                            CrucibleTmpPosition(
+                                position = u.position + u.direction,
+                                u.direction,
+                                u.tilesMovedInDirection + 1
+                            )
+                        )
+                    else ->
+                        turnAround +
+                            CrucibleTmpPosition(
+                                position = u.position + u.direction,
+                                u.direction,
+                                u.tilesMovedInDirection + 1
+                            )
+                }.filter { it.position in nodes }
+            }
+        }
     }
+
+    private operator fun List<String>.get(point: Point2D): Long? =
+        getOrNull(point.y.toInt())?.getOrNull(point.x.toInt())?.digitToInt()?.toLong()
 
     data class CrucibleTmpPosition(
         val position: Point2D,
         val direction: Point2D,
-        val tilesMovedInDirection: Int = 0,
+        val tilesMovedInDirection: Int = 1,
     )
 
     override fun part1(input: List<String>): Any {
         val cityBlocks = CityBlocks(input)
-        val nodes = cityBlocks.nodes
-            .flatMap {
-                listOf(Point2D.LEFT, Point2D.RIGHT, Point2D.UP, Point2D.DOWN).flatMap { direction ->
-                    (0..2).map { tilesMoved ->
-                        CrucibleTmpPosition(it, direction, tilesMoved)
-                    }
-                }
-            }.toSet()
-        val graph = AdjacencyListGraph(nodes) { u ->
-            val leftRight = when (u.direction) {
-                Point2D.UP, Point2D.DOWN -> listOf(Point2D.LEFT, Point2D.RIGHT)
-                Point2D.LEFT, Point2D.RIGHT -> listOf(Point2D.UP, Point2D.DOWN)
-                else -> error("not a valid direction ${u.direction}")
-            }
-            val turnAround = leftRight
-                .map { CrucibleTmpPosition(u.position + it, it) }
-            when (u.tilesMovedInDirection) {
-                2 -> turnAround.filter { it in nodes }
-                else -> (
-                    turnAround +
-                        CrucibleTmpPosition(
-                            position = u.position + u.direction,
-                            u.direction,
-                            u.tilesMovedInDirection + 1
-                        )
-                    ).filter { it in nodes }
-            }
-        }
+        val graph = cityBlocks.buildGraph(1, 3)
         val (distance) = graph.dijkstra(
-            CrucibleTmpPosition(Point2D(0, 0), Point2D.LEFT)
+            CrucibleTmpPosition(Point2D(0, 0), Point2D.RIGHT)
         ) { _, v ->
-            val pos = v.position
-            input[pos.y.toInt()][pos.x.toInt()].digitToInt().toLong()
+            input[v.position]!!
         }
         val bottomRight = Point2D(input[0].indices.last, input.indices.last)
-        return (0..2).flatMap { tiles ->
+        return (1..3).flatMap { tiles ->
             listOf(Point2D.RIGHT, Point2D.DOWN).map { direction ->
                 CrucibleTmpPosition(bottomRight, direction, tiles)
             }
@@ -63,7 +77,19 @@ object Day17 : AdventDay(2023, 17) {
     }
 
     override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
+        val cityBlocks = CityBlocks(input)
+        val graph = cityBlocks.buildGraph(4, 10)
+        val (distance) = graph.dijkstra(
+            CrucibleTmpPosition(Point2D(0, 0), Point2D.RIGHT)
+        ) { _, v ->
+            input[v.position]!!
+        }
+        val bottomRight = Point2D(input[0].indices.last, input.indices.last)
+        return (4..10).flatMap { tiles ->
+            listOf(Point2D.RIGHT, Point2D.DOWN).map { direction ->
+                CrucibleTmpPosition(bottomRight, direction, tiles)
+            }
+        }.minOf { distance(it) ?: Long.MAX_VALUE }
     }
 }
 
--- advent2023/src/test/kotlin/year2023/Day17Test.kt
+++ advent2023/src/test/kotlin/year2023/Day17Test.kt
@@ -6,4 +6,6 @@ import org.junit.jupiter.api.Test
 class Day17Test {
     @Test
     fun part1() = Assertions.assertEquals(102L, Day17.testPart1())
+    @Test
+    fun part2() = Assertions.assertEquals(94L, Day17.testPart2())
 }
