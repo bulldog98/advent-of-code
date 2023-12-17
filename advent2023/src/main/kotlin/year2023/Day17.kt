b6ea3eb5692e28610c141a3bbd4086e7af22b5d0
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-17 07:31:26 +0100

    feat(2023): day 17 part 1
    brute force seems with a bit smaller state space still too slow

--- advent2023/src/main/kotlin/year2023/Day17.kt
+++ advent2023/src/main/kotlin/year2023/Day17.kt
@@ -4,6 +4,8 @@ import AdventDay
 import Point2D
 import graph.AdjacencyListGraph
 import graph.dijkstra
+import kotlin.math.max
+import kotlin.math.min
 
 object Day17 : AdventDay(2023, 17) {
     data class CityBlocks(val input: List<String>) {
@@ -12,54 +14,68 @@ object Day17 : AdventDay(2023, 17) {
         }.toSet()
     }
 
+    private operator fun Point2D.times(factor: Int): Point2D =
+        Point2D(x * factor, y * factor)
+
     data class CrucibleTmpPosition(
         val position: Point2D,
         val direction: Point2D,
-        val tilesMovedInDirection: Int = 0,
-    )
+    ) {
+        fun makeOneMove(): List<CrucibleTmpPosition> = when (direction) {
+            Point2D.UP, Point2D.DOWN -> listOf(Point2D.LEFT, Point2D.RIGHT).flatMap { newDir ->
+                (0..2).map {
+                    CrucibleTmpPosition(position + direction * it + newDir, newDir)
+                }
+            }
+            Point2D.LEFT, Point2D.RIGHT -> listOf(Point2D.UP, Point2D.DOWN).flatMap { newDir ->
+                (0..2).map {
+                    CrucibleTmpPosition(position + direction * it + newDir, newDir)
+                }
+            }
+            else -> error("invalid direction $direction")
+        }
+    }
 
-    override fun part1(input: List<String>): Any {
+    operator fun List<String>.get(point: Point2D): Long? =
+        getOrNull(point.y.toInt())
+            ?.getOrNull(point.x.toInt())
+            ?.digitToInt()
+            ?.toLong()
+
+    operator fun List<String>.get(cruciblePosition: CrucibleTmpPosition): Long? =
+        this[cruciblePosition.position]
+
+    override fun part1(input: List<String>): Long {
         val cityBlocks = CityBlocks(input)
         val nodes = cityBlocks.nodes
             .flatMap {
-                listOf(Point2D.LEFT, Point2D.RIGHT, Point2D.UP, Point2D.DOWN).flatMap { direction ->
-                    (0..2).map { tilesMoved ->
-                        CrucibleTmpPosition(it, direction, tilesMoved)
+                listOf(Point2D.LEFT, Point2D.RIGHT, Point2D.UP, Point2D.DOWN)
+                    .map { direction ->
+                        CrucibleTmpPosition(it, direction)
                     }
-                }
             }.toSet()
         val graph = AdjacencyListGraph(nodes) { u ->
-            val leftRight = when (u.direction) {
-                Point2D.UP, Point2D.DOWN -> listOf(Point2D.LEFT, Point2D.RIGHT)
-                Point2D.LEFT, Point2D.RIGHT -> listOf(Point2D.UP, Point2D.DOWN)
-                else -> error("not a valid direction ${u.direction}")
-            }
-            val turnAround = leftRight
-                .map { CrucibleTmpPosition(u.position + it, it) }
-            when (u.tilesMovedInDirection) {
-                2 -> turnAround.filter { it in nodes }
-                else -> (
-                    turnAround +
-                        CrucibleTmpPosition(
-                            position = u.position + u.direction,
-                            u.direction,
-                            u.tilesMovedInDirection + 1
-                        )
-                    ).filter { it in nodes }
-            }
+            u.makeOneMove().filter { it in nodes }
         }
         val (distance) = graph.dijkstra(
-            CrucibleTmpPosition(Point2D(0, 0), Point2D.LEFT)
-        ) { _, v ->
-            val pos = v.position
-            input[pos.y.toInt()][pos.x.toInt()].digitToInt().toLong()
+            CrucibleTmpPosition(Point2D(0, 0), Point2D.RIGHT)
+        ) { u, v ->
+            val costForVItself = input[v] ?: error("v $v has to be in input")
+            val costForUItself = input[u] ?: error("u $u has to be in input")
+            return@dijkstra costForVItself - costForUItself + when (u.direction) {
+                Point2D.LEFT, Point2D.RIGHT -> (min(v.position.x, u.position.x)..max(v.position.x, u.position.x)).sumOf { x ->
+                    input[u.position.y.toInt()][x.toInt()].digitToInt().toLong()
+                }
+                Point2D.UP, Point2D.DOWN -> (min(v.position.y, u.position.y) ..max(v.position.y, u.position.y)).sumOf { y ->
+                    input[y.toInt()][u.position.x.toInt()].digitToInt().toLong()
+                }
+                else -> error("invalid direction ${u.direction}")
+            }
         }
         val bottomRight = Point2D(input[0].indices.last, input.indices.last)
-        return (0..2).flatMap { tiles ->
-            listOf(Point2D.RIGHT, Point2D.DOWN).map { direction ->
-                CrucibleTmpPosition(bottomRight, direction, tiles)
-            }
-        }.minOf { distance(it) ?: Long.MAX_VALUE }
+        return listOf(Point2D.RIGHT, Point2D.DOWN).map { direction ->
+                CrucibleTmpPosition(bottomRight, direction)
+            }.minOf { distance(it) ?: Long.MAX_VALUE }
     }
 
     override fun part2(input: List<String>): Any {
