27c2ee51312e604bcb141074b08d3ba6128aed86
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-18 06:43:58 +0100

    feat(2023): solve day 18 part 1

---
+++ advent2023/src/test/kotlin/year2023/Day18Test.kt
@@ -0,0 +1,9 @@
+package year2023
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day18Test {
+    @Test
+    fun part1() = Assertions.assertEquals(62, Day18.testPart1())
+}
