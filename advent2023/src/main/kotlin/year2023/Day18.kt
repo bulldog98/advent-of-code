30e270db2e952965db67b2820eaadf73f5e2e660
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-18 06:47:53 +0100

    refactor(2023): day 18 part 1
    extract common logic for part 2

--- advent2023/src/main/kotlin/year2023/Day18.kt
+++ advent2023/src/main/kotlin/year2023/Day18.kt
@@ -3,10 +3,11 @@ package year2023
 import AdventDay
 import Point2D
 
-object Day18: AdventDay(2023, 18) {
+object Day18 : AdventDay(2023, 18) {
     operator fun Point2D.times(times: Int): Point2D =
         Point2D(x * times, y * times)
-    private fun parseInstruction(input: String): Pair<Point2D, Int> = when (input.first()) {
+
+    private fun parseInstructionPart1(input: String): Pair<Point2D, Int> = when (input.first()) {
         'R' -> Point2D.RIGHT
         'L' -> Point2D.LEFT
         'U' -> Point2D.UP
@@ -14,21 +15,20 @@ object Day18: AdventDay(2023, 18) {
         else -> error("unknown direction")
     } to input.split(" ")[1].toInt()
 
-    override fun part1(input: List<String>): Int {
-        val instructions = input.map(::parseInstruction)
-        val currentPosition = Point2D(0, 0)
-        val walls = instructions.fold(setOf(currentPosition) to currentPosition) { (visited, curr), (dir, times) ->
+    private fun List<Pair<Point2D, Int>>.computeWall() =
+        fold(setOf(Point2D(0, 0)) to Point2D(0, 0)) { (visited, curr), (dir, times) ->
             val newVisits = (0 until times).runningFold(curr) { cur, _ ->
                 cur + dir
             }
             visited + newVisits to newVisits.last()
         }.first
-        val alreadyHollow = walls.size
-        val topLeftOutside = Point2D(walls.minOf { it.x } - 1, walls.minOf { it.y } - 1)
-        val bottomRightOutside = Point2D(walls.maxOf { it.x } + 1, walls.maxOf { it.y } + 1)
+
+    private fun Set<Point2D>.countPointsNotFloodedFromOutside(): Int {
+        val topLeftOutside = Point2D(minOf { it.x } - 1, minOf { it.y } - 1)
+        val bottomRightOutside = Point2D(maxOf { it.x } + 1, maxOf { it.y } + 1)
         val xRange = (topLeftOutside.x..bottomRightOutside.x)
         val yRange = (topLeftOutside.y..bottomRightOutside.y)
-        val visited = walls.toMutableSet()
+        val visited = toMutableSet()
         val queue = mutableListOf(topLeftOutside)
         while (queue.isNotEmpty()) {
             val current = queue.removeFirst()
@@ -37,9 +37,14 @@ object Day18: AdventDay(2023, 18) {
                 it.x in xRange && it.y in yRange && it !in visited && it !in queue
             }
         }
-        return xRange.sumOf { x ->
-            yRange.count { y -> Point2D(x, y) !in visited }
-        } + alreadyHollow
+        return xRange.count() * yRange.count() - visited.size
+    }
+
+    override fun part1(input: List<String>): Int {
+        val instructions = input.map(::parseInstructionPart1)
+        val walls = instructions.computeWall()
+        val alreadyHollow = walls.size
+        return walls.countPointsNotFloodedFromOutside() + alreadyHollow
     }
 
     override fun part2(input: List<String>): Any {
