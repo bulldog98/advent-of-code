5b9b4f7dbfbf836a33b3ae7b760b7f749eace5c6
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-20 10:44:02 +0100

    refactor(2023): day 14
    make tilting generic in direction

--- advent2023/src/main/kotlin/year2023/Day14.kt
+++ advent2023/src/main/kotlin/year2023/Day14.kt
@@ -21,25 +21,43 @@ object Day14 : AdventDay(2023, 14) {
         val load: Long
             get() = roundRocks.sumOf { yRange.last - it.y + 1 }
 
-        fun tiltNorth(): Dish {
-            val newRoundRockPositions: Set<Point2D> = xRange.flatMap { x ->
-                val rocksInThisRow = roundRocks.filter { it.x == x }.sortedBy { it.y }
-                val wallsInThisRow = (cubeRocks.filter { it.x == x } + Point2D(x, -1) + Point2D(x, yRange.last + 1)).sortedByDescending { it.y }
-                if (wallsInThisRow.size == 1)
-                     Point2D(x, -1).setCountInDirection(rocksInThisRow.size, Point2D.DOWN)
-                else {
-                    val lookup = wallsInThisRow
-                        .associateWith { wall ->
-                            rocksInThisRow.count { it.y > wall.y }
-                        }
-                    wallsInThisRow.zipWithNext().filter { (a, b) ->
-                        lookup[a] != lookup[b]
-                    }.flatMap { (oldPoint, point) ->
-                        point.setCountInDirection(lookup[point]!! - lookup[oldPoint]!!, Point2D.DOWN)
+        private fun LongRange.tiltWithRow(
+            direction: Point2D,
+            getRow: Point2D.() -> Long,
+            getColumn: Point2D.() -> Long,
+            sortingFunction: List<Point2D>.(Point2D.() -> Long) -> List<Point2D>
+        ): Dish {
+            val newRoundRockPositions: Set<Point2D> = this.flatMap { coord ->
+                val rocksInThisRow = roundRocks.filter { it.getRow() == coord }.sortedBy(getColumn)
+                val wallsInThisRow = (cubeRocks.filter { it.getRow() == coord }).sortedByDescending(getColumn)
+                val lookup = wallsInThisRow
+                    .associateWith { wall ->
+                        rocksInThisRow.count { it.getColumn() > wall.getColumn() }
                     }
+                wallsInThisRow.zipWithNext().filter { (a, b) ->
+                    lookup[a] != lookup[b]
+                }.flatMap { (oldPoint, point) ->
+                    point.setCountInDirection(lookup[point]!! - lookup[oldPoint]!!, direction)
                 }
             }.toSet()
             return copy(roundRocks = newRoundRockPositions)
+        }
+
+
+        fun tiltInDirection(direction: Point2D): Dish = when (direction) {
+            Point2D.UP, Point2D.DOWN -> xRange.tiltWithRow(direction, Point2D::x, Point2D::y) {
+                if (direction == Point2D.DOWN) {
+                    sortedByDescending(it)
+                } else
+                    sortedBy(it)
+            }
+            Point2D.LEFT, Point2D.RIGHT -> yRange.tiltWithRow(direction, Point2D::y, Point2D::x) {
+                if (direction == Point2D.RIGHT) {
+                    sortedByDescending(it)
+                } else
+                    sortedBy(it)
+            }
+            else -> error("that is not one of cardinal directions")
         }
 
         fun prettyPrint(): List<String> = yRange.map { y ->
@@ -59,14 +77,25 @@ object Day14 : AdventDay(2023, 14) {
                     0L..<input.size.toLong(),
                     input.findAllPositionsOf('#'),
                     input.findAllPositionsOf('O')
-                )
+                ).let {
+                    it.copy(
+                        cubeRocks = it.cubeRocks
+                            + it.yRange.flatMap { y ->
+                            listOf(-1, it.xRange.last + 1)
+                                .map { x -> Point2D(x, y) }
+                        } + it.xRange.flatMap { x ->
+                            listOf(-1, it.yRange.last + 1)
+                                .map { y -> Point2D(x, y) }
+                        }
+                    )
+                }
         }
     }
 
     override fun part1(input: List<String>): Long =
         Dish.of(input)
-            .tiltNorth()
-            // .also { it.prettyPrint().forEach(::println) }
+            .tiltInDirection(Point2D.DOWN)
+            .also { it.prettyPrint().forEach(::println) }
             .load
 
     override fun part2(input: List<String>): Any {
