b43feb91f529a35bc3816d08738d31f6b90ac35c
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-25 08:57:56 +0100

    feat(2023): solve day 15 part 2

--- advent2023/src/main/kotlin/year2023/Day15.kt
+++ advent2023/src/main/kotlin/year2023/Day15.kt
@@ -3,19 +3,55 @@ package year2023
 import AdventDay
 
 object Day15: AdventDay(2023, 15) {
-    fun String.hashWithAppendix1A(): Int {
+    private fun String.hashWithAppendix1A(): Int {
         var res = 0
         forEach { c ->
             res = ((res + c.code) * 17) % 256
         }
         return res
     }
+    data class Lens(val label: String, val lensPower: Int)
+
+    sealed interface Instruction {
+        val label: String
+        data class Dash(override val label: String): Instruction
+        data class Equals(override val label: String, val lensPower: Int): Instruction
+
+        companion object {
+            fun of(input: String) = when {
+                "=" in input -> Equals(input.substringBefore("="), input.substringAfter("=").toInt())
+                else -> Dash(input.substringBefore("-"))
+            }
+        }
+    }
+
     override fun part1(input: List<String>): Int {
         return input[0].split(",").sumOf { it.hashWithAppendix1A() }
     }
 
     override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
+        val instructions = input[0].split(",").map {
+            Instruction.of(it)
+        }
+        val boxes = Array(256) { mutableListOf<Lens>() }
+        instructions.forEach { inst ->
+            val hash = inst.label.hashWithAppendix1A()
+            when (inst) {
+                is Instruction.Equals -> {
+                    val lens = Lens(inst.label, inst.lensPower)
+                    val index = boxes[hash].indexOfFirst { it.label == inst.label }
+                    if (index > -1)
+                        boxes[hash][index] = lens
+                    else
+                        boxes[hash] += lens
+                }
+                is Instruction.Dash ->
+                    boxes[hash].removeIf { it.label == inst.label }
+            }
+        }
+        return boxes.flatMapIndexed { i: Int, box: MutableList<Lens> ->
+            box.mapIndexed { j, lens -> (1 + i) * (1 + j) * lens.lensPower }
+        }.sum()
     }
 }
 
--- advent2023/src/test/kotlin/year2023/Day15Test.kt
+++ advent2023/src/test/kotlin/year2023/Day15Test.kt
@@ -6,4 +6,6 @@ import org.junit.jupiter.api.Test
 class Day15Test {
     @Test
     fun part1() = Assertions.assertEquals(1320, Day15.testPart1())
+    @Test
+    fun part2() = Assertions.assertEquals(145, Day15.testPart2())
 }
