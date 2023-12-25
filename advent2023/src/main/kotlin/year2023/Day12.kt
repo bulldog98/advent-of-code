7deb3b3d2412c51dad583110b1aa37372b17b41c
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-25 09:35:39 +0100

    feat(2023): solve day 12 part 2
    using dynamic programming

--- advent2023/src/main/kotlin/year2023/Day12.kt
+++ advent2023/src/main/kotlin/year2023/Day12.kt
@@ -4,30 +4,7 @@ import AdventDay
 import helper.numbers.parseAllInts
 
 object Day12 : AdventDay(2023, 12) {
-    fun String.replaceFirstChar(char: Char, with: List<Char>): List<String> =
-        when (firstOrNull()) {
-            null -> listOf("")
-            '?' -> drop(1).replaceFirstChar(char, with).flatMap { listOf(".$it", "#$it") }
-            else -> drop(1).replaceFirstChar(char, with).map { "${first()}$it" }
-        }
-
-    fun String.computeNumRepresentation(cur: Int = 0, inGroup: Boolean = false): List<Int> =
-        when (firstOrNull()) {
-            null -> if (cur != 0) listOf(cur) else emptyList()
-            '.' ->
-                if (inGroup)
-                    listOf(cur) + drop(1).computeNumRepresentation()
-                else
-                    drop(1).computeNumRepresentation()
-            '#' ->
-                if (inGroup)
-                    drop(1).computeNumRepresentation(cur + 1, true)
-                else
-                    drop(1).computeNumRepresentation(1, true)
-            else -> error("can not handle ${first()}")
-        }
-
-    data class Line(val charRepresentation: String, val numRepresentation: List<Int>) {
+    data class Line(val pattern: String, val numRepresentation: List<Int>) {
         companion object {
             fun of(input: String): Line {
                 val (first, second) = input.split(" ")
@@ -35,22 +12,114 @@ object Day12 : AdventDay(2023, 12) {
             }
         }
 
-        fun findAllPossible(): List<String> {
-            return charRepresentation
-                .replaceFirstChar('?', listOf('#', '.'))
-                .filter { it.computeNumRepresentation() == numRepresentation }
+        private fun asState() = State(
+            pattern.length + 1,
+            mask,
+            pattern
+        )
+
+        private class State(val n: Int, val mask: BooleanArray, val pattern: String, val dpPoint: Array<LongArray>, val dpHash: Array<LongArray>) {
+            constructor(n: Int, mask: BooleanArray, pattern: String): this(
+                n,
+                mask,
+                pattern,
+                Array(n) { LongArray(mask.size) { 0 } }.also {
+                    it[0][0] = 1
+                },
+                Array(n) { LongArray(mask.size) { 0 } }.also {
+                    it[0][0] = 1
+                }
+            )
+            private val m: Int
+                get() = mask.size
+            private fun considerPoint(i: Int, j: Int) {
+                if (!mask[j]) {
+                    dpPoint[i][j] += dpPoint[i - 1][j]
+                    dpHash[i][j] += dpPoint[i - 1][j]
+                }
+            }
+
+            private fun considerBroken(i: Int, j: Int) {
+                if (mask[j]) {
+                    dpPoint[i][j] += dpHash[i - 1][j - 1]
+                    dpHash[i][j] += dpHash[i - 1][j - 1]
+                } else {
+                    dpPoint[i][j] += dpHash[i][j - 1]
+                }
+            }
+
+            fun computeNumberOfArrangements(): Long {
+                for (i in 1 ..< n) {
+                    val c = pattern[i - 1]
+                    if (c == '.' || c == '?') {
+                        dpPoint[i][0] = dpPoint[i - 1][0]
+                        dpHash[i][0] = dpHash[i - 1][0]
+                    }
+
+                    for (j in 1 ..< m) {
+                        when(c) {
+                            '.' -> {
+                                considerPoint(i, j)
+                            }
+                            '#' -> {
+                                considerBroken(i, j)
+                            }
+                            '?' -> {
+                                considerPoint(i, j)
+                                considerBroken(i, j)
+                            }
+                        }
+                    }
+                }
+
+                return dpPoint.last().last()
+            }
+        }
+
+        private val mask: BooleanArray by lazy {
+            val sum = numRepresentation.sum()
+            val n = numRepresentation.size
+
+            val mask = BooleanArray(n + sum + 1) { false }
+            var ctr = 0
+            for (i in 0 ..< n) {
+                repeat(numRepresentation[i]) {
+                    ++ctr
+                    mask[ctr] = true
+                }
+                ++ctr
+            }
+
+            mask
+        }
+
+        fun computeNumberOfArrangements() =
+            asState().computeNumberOfArrangements()
+    }
+
+    private fun <T> List<T>.repeatList(n: Int): List<T> = buildList {
+        repeat(n) {
+            addAll(this@repeatList)
         }
     }
 
-    override fun part1(input: List<String>): Int =
+    override fun part1(input: List<String>): Long =
         input
             .map(Line::of)
-            .map { it.findAllPossible() }
-            .sumOf { it.size }
+            .sumOf { it.computeNumberOfArrangements() }
 
-    override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
-    }
+
+    override fun part2(input: List<String>): Long =
+        input
+            .map { line ->
+                val (pattern, nums) = line.split(" ")
+                val newPattern = listOf(pattern).repeatList(5).joinToString("?") { it }
+                val newNums = listOf(nums).repeatList(5).joinToString(",") { it }
+                "$newPattern $newNums"
+            }.map(Line::of)
+            .sumOf {
+                it.computeNumberOfArrangements()
+            }
 }
 
 fun main() = Day12.run()
--- advent2023/src/test/kotlin/year2023/Day12Test.kt
+++ advent2023/src/test/kotlin/year2023/Day12Test.kt
@@ -5,5 +5,7 @@ import org.junit.jupiter.api.Test
 
 class Day12Test {
     @Test
-    fun part1() = Assertions.assertEquals(21, Day12.testPart1())
+    fun part1() = Assertions.assertEquals(21L, Day12.testPart1())
+    @Test
+    fun part2() = Assertions.assertEquals(525152L, Day12.testPart2())
 }
