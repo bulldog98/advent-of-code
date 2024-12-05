9c93f5c4dc1178c6c76374f3f0394de99b9760d5
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-05 06:22:35 +0100

    refactor(2024): cleanup day 5

--- advent2024/src/main/kotlin/year2024/Day05.kt
+++ advent2024/src/main/kotlin/year2024/Day05.kt
@@ -4,8 +4,8 @@ import AdventDay
 import helper.numbers.toAllLongs
 
 fun List<String>.getPageOrderRules(): List<Pair<Long, Long>> = takeWhile { it.isNotEmpty() }
-    .map {
-        val (a, b) = it.split("|").map { it.toLong() }
+    .map { orderRule ->
+        val (a, b) = orderRule.split("|").map { it.toLong() }
         a to b
     }
 
@@ -13,16 +13,20 @@ fun List<String>.getPageOrderings(): List<List<Long>> = dropWhile { it.isNotEmpt
     .drop(1)
     .map { it.toAllLongs().toList() }
 
+class PageOrderRuleComparator(private val pageOrderRules: List<Pair<Long, Long>>) : Comparator<Long> {
+    override fun compare(x: Long?, y: Long?): Int = when {
+        x == y -> 0
+        x to y in pageOrderRules -> -1
+        else -> 1
+    }
+
+}
+
 private fun List<Long>.orderByRules(orderRules: List<Pair<Long, Long>>): List<Long> {
     val applyingRules = orderRules.filter { (a, b) -> a in this && b in this }
+    val pageOrderRuleComparator = PageOrderRuleComparator(applyingRules)
 
-    return this.sortedWith { x, y ->
-        when {
-            x == y -> 0
-            x to y in applyingRules -> -1
-            else -> 1
-        }
-    }
+    return this.sortedWith(pageOrderRuleComparator)
 }
 
 object Day05 : AdventDay(2024, 5) {
