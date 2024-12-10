5143968db97d5a4022a67b59d70c8cfd0cf57918
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-10 06:39:36 +0100

    refactor(2024): cleanup day 10

--- advent2024/src/main/kotlin/year2024/Day10.kt
+++ advent2024/src/main/kotlin/year2024/Day10.kt
@@ -7,48 +7,48 @@ import graph.AdjacencyListGraph
 import graph.Graph
 import graph.reachableNodesFrom
 
-fun List<String>.allPositions() = this[0].indices.flatMap { x ->
-    this.indices.map { y -> Point2D(x, y) }
+data class HikeMap(private val input: List<String>) {
+    private val startPoints = input.findAllPositionsOf('0')
+    private val xRange = input[0].indices
+    private val yRange = input.indices
+    private val graph = input.computeGraph(xRange, yRange)
+
+    private fun List<String>.allPositions() = this[0].indices.flatMap { x ->
+        this.indices.map { y -> Point2D(x, y) }
+    }
+
+    private fun List<String>.computeGraph(xRange: IntRange, yRange: IntRange) = AdjacencyListGraph(this.allPositions()) {
+        val start = this[it]
+        it.cardinalNeighbors.filter { neighbor ->
+            neighbor.x in xRange && neighbor.y in yRange &&
+                start.digitToInt() + 1 == this[neighbor].digitToInt()
+        }
+    }
+
+    private operator fun List<String>.get(point2D: Point2D) = this[point2D.y.toInt()][point2D.x.toInt()]
+
+    private fun Graph<Point2D>.computeRatingFor(start: Point2D) = buildList<List<Point2D>> {
+        add(listOf(start))
+        while (this.any { neighborsOf(it.last()).isNotEmpty() }) {
+            val way = this.first { neighborsOf(it.last()).isNotEmpty() }
+            this -= way
+            this.addAll(neighborsOf(way.last()).map { way + it })
+        }
+    }.count { input[it.last()] == '9' }
+
+    fun computeRating() = startPoints.sumOf { graph.computeRatingFor(it) }
+
+    private fun Graph<Point2D>.computeScoreFor(start: Point2D) = reachableNodesFrom(start)
+        .count { input[it] == '9' }
+
+    fun computeScore() = startPoints.sumOf { graph.computeScoreFor(it) }
 }
 
-operator fun List<String>.get(point2D: Point2D) = this[point2D.y.toInt()][point2D.x.toInt()]
-
-fun List<String>.computeGraph(xRange: IntRange, yRange: IntRange) = AdjacencyListGraph(this.allPositions()) {
-    val start = this[it]
-    it.cardinalNeighbors.filter { neighbor ->
-        neighbor.x in xRange && neighbor.y in yRange &&
-            start.digitToInt() + 1 == this[neighbor].digitToInt()
-    }
-}
-
-fun Graph<Point2D>.computeScoreFor(start: Point2D, isEndPoint: (Point2D) -> Boolean) = reachableNodesFrom(start)
-    .count { reached -> isEndPoint(reached) }.toLong().also { res -> println("$start reaches $res 9s") }
-
-fun Graph<Point2D>.computeRatingFor(start: Point2D, isEndPoint: (Point2D) -> Boolean) = buildList<List<Point2D>> {
-    add(listOf(start))
-    while (this.any { neighborsOf(it.last()).isNotEmpty() }) {
-        val way = this.first { neighborsOf(it.last()).isNotEmpty() }
-        this -= way
-        this.addAll(neighborsOf(way.last()).map { way + it })
-    }
-}.count { isEndPoint(it.last()) }
 
 object Day10 : AdventDay(2024, 10) {
-    override fun part1(input: List<String>): Long {
-        val startPoints = input.findAllPositionsOf('0')
-        val xRange = input[0].indices
-        val yRange = input.indices
-        val graph = input.computeGraph(xRange, yRange)
-        return startPoints.sumOf { graph.computeScoreFor(it) { end -> input[end] == '9' } }
-    }
+    override fun part1(input: List<String>) = HikeMap(input).computeScore()
 
-    override fun part2(input: List<String>): Long {
-        val startPoints = input.findAllPositionsOf('0')
-        val xRange = input[0].indices
-        val yRange = input.indices
-        val graph = input.computeGraph(xRange, yRange)
-        return startPoints.sumOf { start -> graph.computeRatingFor(start) { end -> input[end] == '9' }.toLong() }
-    }
+    override fun part2(input: List<String>) = HikeMap(input).computeRating()
 }
 
 fun main() = Day10.run()
--- advent2024/src/test/kotlin/year2024/Day10Test.kt
+++ advent2024/src/test/kotlin/year2024/Day10Test.kt
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test
 class Day10Test {
     private val day = Day10
     @Test
-    fun part1() = assertEquals(36L, day.testPart1())
+    fun part1() = assertEquals(36, day.testPart1())
 
     @Test
-    fun part2() = assertEquals(81L, day.testPart2())
+    fun part2() = assertEquals(81, day.testPart2())
 }
