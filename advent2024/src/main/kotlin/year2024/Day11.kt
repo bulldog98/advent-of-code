e71c7b52e556bf5807d7d0a2901fe4bc0703635f
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-11 07:00:47 +0100

    refactor(2024): cleanup day 11

--- advent2024/src/main/kotlin/year2024/Day11.kt
+++ advent2024/src/main/kotlin/year2024/Day11.kt
@@ -20,21 +20,18 @@ data class CacheEntry(val stone: Long, val step: Int, val limit: Int)
 object Day11 : AdventDay(2024, 11) {
     private val cache = mutableMapOf<CacheEntry, Long>()
     private fun countStones(stone: Long, round: Int, limit: Int): Long = cache.getOrPut(CacheEntry(stone, round, limit)) {
-        if (round == limit) 1 else stone.oneStep().sumOf { countStones(it, round + 1, limit) }
+        if (round == limit)
+            1
+        else
+            stone.oneStep().sumOf { countStones(it, round + 1, limit) }
     }
 
-    override fun part1(input: List<String>): Long {
-        val startingStones = input[0].toAllLongs()
-        return startingStones.sumOf {
-            countStones(it, 0, 25)
-        }
+    override fun part1(input: List<String>) = input[0].toAllLongs().sumOf {
+        countStones(it, 0, 25)
     }
 
-    override fun part2(input: List<String>): Long {
-        val startingStones = input[0].toAllLongs()
-        return startingStones.sumOf {
-            countStones(it, 0, 75)
-        }
+    override fun part2(input: List<String>) = input[0].toAllLongs().sumOf {
+        countStones(it, 0, 75)
     }
 }
 
