84aab7e38b7cc6a0ab352d73d1cd3f9bff00d7ac
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-18 07:02:18 +0100

    refactor(2024): day 18 use binary search

--- advent2024/src/main/kotlin/year2024/Day18.kt
+++ advent2024/src/main/kotlin/year2024/Day18.kt
@@ -26,16 +26,30 @@ class Day18(
         return cost(destination) ?: Long.MAX_VALUE
     }
 
+    private fun IntRange.binarySearch(isValid: (Int) -> Boolean): IntRange = when {
+        isEmpty() -> this
+        first == last -> this
+        else -> {
+            val middle = first + (last - first) / 2 + 1
+            val restToSearch = if (isValid(middle)) {
+                (middle..last)
+            } else {
+                (first..<middle)
+            }
+            restToSearch.binarySearch(isValid)
+        }
+    }
+
     override fun part1(input: InputRepresentation): Long {
         return input.map { it.toAllLongs().toList() }.map { Point2D(it[0], it[1]) }.take(bytesForPart1).costForTravelToEndPoint()
     }
 
     override fun part2(input: InputRepresentation): String {
         val bytesToCorrupt = input.map { it.toAllLongs().toList() }.map { Point2D(it[0], it[1]) }
-        val indexOfBlockingByte = (bytesForPart1 ..< bytesToCorrupt.size).first {
-            println("search with $it bytes corrupted")
-            bytesToCorrupt.take(it).costForTravelToEndPoint() == Long.MAX_VALUE
-        } - 1
+        // search backwards, since it's nearer the end
+        val indexOfBlockingByte = (bytesForPart1..<bytesToCorrupt.size).binarySearch {
+            bytesToCorrupt.take(it).costForTravelToEndPoint() != Long.MAX_VALUE
+        }.first
         return bytesToCorrupt[indexOfBlockingByte].let {
             "${it.x},${it.y}"
         }
