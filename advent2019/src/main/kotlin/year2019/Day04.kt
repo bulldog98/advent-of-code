a323b364c5f7096102efe4872c94165d2ba5dabf
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-18 09:46:04 +0100

    feat(2019): solve day 4 part 2

--- advent2019/src/main/kotlin/year2019/Day04.kt
+++ advent2019/src/main/kotlin/year2019/Day04.kt
@@ -4,20 +4,37 @@ import adventday.AdventDay
 import adventday.InputRepresentation
 
 object Day04: AdventDay(2019, 4) {
-    fun Long.testPwCriteria() = toString().toList().let { digits ->
-        digits.zipWithNext().all { (a, b) -> a <= b } &&
-            digits.zipWithNext().any { (a, b) -> a == b }
+    fun List<Char>.testPwCriteriaPart1() = allNotDecreasing() && zipWithNext().any { (a, b) -> a == b }
+    fun List<Char>.testPwCriteriaPart2() = allNotDecreasing() && hasGroupOfExactLength2()
+
+    private fun List<Char>.allNotDecreasing() = zipWithNext().all { (a, b) -> a <= b }
+
+    private fun List<Char>.hasGroupOfExactLength2(): Boolean {
+        var rest = this.toList()
+        while (rest.isNotEmpty()) {
+            val charToRemove = rest.first()
+            val newRest = rest.dropWhile { it == charToRemove }
+            if (rest.size - newRest.size == 2) return true
+            rest = newRest
+        }
+        return false
     }
+
+    fun Long.toDigits() = toString().toList()
 
     override fun part1(input: InputRepresentation): Int {
         val range = input[0].split("-").map { it.toLong() }.let { it[0]..it[1] }
         return range.count {
-            it.testPwCriteria()
+            it.toDigits().testPwCriteriaPart1()
         }
     }
 
-    override fun part2(input: InputRepresentation): Any {
-        TODO("Not yet implemented")
+    override fun part2(input: InputRepresentation): Int {
+        val range = input[0].split("-").map { it.toLong() }.let { it[0]..it[1] }
+
+        return range.count {
+                it.toDigits().testPwCriteriaPart2()
+            }
     }
 }
 
--- advent2019/src/test/kotlin/year2019/Day04Test.kt
+++ advent2019/src/test/kotlin/year2019/Day04Test.kt
@@ -1,4 +1,37 @@
 package year2019
 
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import year2019.Day04.testPwCriteriaPart1
+import year2019.Day04.testPwCriteriaPart2
+import year2019.Day04.toDigits
+
 class Day04Test {
+    @Nested
+    inner class Part1 {
+        @Test
+        fun `first example`() = assertTrue((111111L).toDigits().testPwCriteriaPart1())
+
+        @Test
+        fun `second example`() = assertFalse((223450L).toDigits().testPwCriteriaPart1())
+
+        @Test
+        fun `third example`() = assertFalse((123789L).toDigits().testPwCriteriaPart1())
+    }
+    @Nested
+    inner class Part2 {
+        @Test
+        fun `first example`() = assertTrue((112233L).toDigits().testPwCriteriaPart2())
+
+        @Test
+        fun `second example`() = assertFalse((123444L).toDigits().testPwCriteriaPart2())
+
+        @Test
+        fun `third example`() = assertTrue((111122L).toDigits().testPwCriteriaPart2())
+
+        @Test
+        fun `own example found by bug`() = assertFalse((111112L).toDigits().testPwCriteriaPart2())
+    }
 }
