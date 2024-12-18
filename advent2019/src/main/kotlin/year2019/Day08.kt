db9a7d982f8b1effebcfbf1e1413170852d223a5
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-18 18:52:31 +0100

    feat(2019): solve day 8 part 2

--- advent2019/src/main/kotlin/year2019/Day08.kt
+++ advent2019/src/main/kotlin/year2019/Day08.kt
@@ -1,25 +1,54 @@
 package year2019
 
+import Point2D
 import adventday.AdventDay
 import adventday.InputRepresentation
 
-class Day08(val width: Int, val height: Int): AdventDay(2019, 8) {
-    val digitRegex = """\d""".toRegex()
-
-    override fun part1(input: InputRepresentation): Int {
-        digitRegex.findAll(input.asText()).map {
-            it.value.toInt()
+class Day08(
+    private val width: Int,
+    private val height: Int
+) : AdventDay(2019, 8) {
+    private data class Layer(val rows: List<List<Int>>) {
+        fun countOfDigits(digit: Int) = rows.sumOf { row ->
+            row.count { it == digit }
         }
-        val layers = """\d""".toRegex().findAll(input.asText()).map { it.value.toInt() }.chunked(width * height) {
-            it.chunked(width)
-        }.toList()
 
-        val layerWithFewest0s = layers.minBy { it.sumOf { it.count { it == 0 } } }
-        return layerWithFewest0s.sumOf { it.count { it == 1 } } * layerWithFewest0s.sumOf { it.count { it == 2 } }
+        operator fun get(point: Point2D) = rows[point.y.toInt()][point.x.toInt()]
+
+        companion object {
+            private val digitRegex = """\d""".toRegex()
+            fun parseInput(input: InputRepresentation, width: Int, height: Int): List<Layer> =
+                digitRegex.findAll(input.asText()).map { it.value.toInt() }.chunked(width * height) {
+                    Layer(it.chunked(width))
+                }.toList()
+        }
     }
 
-    override fun part2(input: InputRepresentation): Long {
-        TODO("Not yet implemented")
+    private operator fun List<Layer>.get(point: Point2D) = fold(2) { currentPixel, nextLayer ->
+        when (currentPixel) {
+            2 -> nextLayer[point]
+            else -> currentPixel
+        }
+    }
+
+    override fun part1(input: InputRepresentation): Int {
+        val layers = Layer.parseInput(input, width, height)
+
+        val layerWithFewest0s = layers.minBy { it.countOfDigits(0) }
+        return layerWithFewest0s.countOfDigits(1) * layerWithFewest0s.countOfDigits(2)
+    }
+
+    override fun part2(input: InputRepresentation): String {
+        val layers = Layer.parseInput(input, width, height)
+
+        val result = (0..<height).joinToString("\n") { y ->
+            (0..<width).joinToString("") { x ->
+                layers[Point2D(x, y)].toString()
+            }
+        }
+
+        // have to squint at the output to see the chars that they represent
+        return result
     }
 }
 
--- advent2019/src/test/kotlin/year2019/Day08Test.kt
+++ advent2019/src/test/kotlin/year2019/Day08Test.kt
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day08Test {
-    private val day = Day08(3, 2)
+    private val dayForPart1 = Day08(3, 2)
+    private val dayForPart2 = Day08(2, 2)
 
     @Test
-    fun part1() = assertEquals(1, day.testPart1())
+    fun part1() = assertEquals(1, dayForPart1.testPart1())
+
+
+    @Test
+    fun part2() = assertEquals("01\n10", dayForPart2.testPart2("test2"))
 }
