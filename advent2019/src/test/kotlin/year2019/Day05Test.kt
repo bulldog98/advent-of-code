0f1ca71cba03f55293eb04e2f1be32a22ba4e1a1
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2024-12-18 11:10:45 +0100

    feat(2019): day 5 add part 1 examples

--- advent2019/src/test/kotlin/year2019/Day05Test.kt
+++ advent2019/src/test/kotlin/year2019/Day05Test.kt
@@ -1,20 +1,31 @@
 package year2019
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import year2019.computer.IntComputer
 
 class Day05Test {
-    private val day = Day05
+    @Nested
+    inner class Part1 {
+        @Test
+        fun `example that explains parameter modes`() {
+            val exampleComputer = IntComputer.parse("1002,4,3,4,33")
+            assertEquals(
+                listOf<Long>(
+                    1002,4,3,4,99
+                ),
+                exampleComputer.computeOneStep()[0..4L]
+            )
+        }
 
-    @Test
-    fun part1() {
-        val exampleComputer = IntComputer.parse("1002,4,3,4,33")
-        assertEquals(
-            listOf<Long>(
-                1002,4,3,4,99
-            ),
-            exampleComputer.computeOneStep()[0..4L]
-        )
+        @Test
+        fun `negative additions work`() {
+            val exampleComputer = IntComputer.parse("1101,100,-1,4,0")
+            assertEquals(
+                99,
+                exampleComputer.computeOneStep()[4]
+            )
+        }
     }
 }
