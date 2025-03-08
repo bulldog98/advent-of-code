8aeb3b0b8a00b246a4e7b380f0d247d4f307ed81
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-03-08 22:01:39 +0100

    feat(2021): solve day 12 part 2

--- advent2021/src/main/kotlin/year2021/Day12.kt
+++ advent2021/src/main/kotlin/year2021/Day12.kt
@@ -18,30 +18,40 @@ object Day12 : AdventDay(2021, 12) {
     private data class PathFragment(
         val currentNode: String,
         val hash: Int,
+        val allowedToVisitSmallCaveAgain: Boolean = false,
         val visited: Set<String> = setOf(currentNode)
     )
 
     private val cache = mutableMapOf<PathFragment, Long>()
-    private fun Graph<String>.pathsFrom(from: PathFragment): Long = with(cache) {
-        getOrPut(from) {
-            when (from.currentNode) {
-                "end" -> 1
-                else -> neighborsOf(from.currentNode).filter {
-                    it.all { char -> char.isUpperCase() } || it !in from.visited
-                }.sumOf {
-                    pathsFrom(from.copy(currentNode = it, visited = from.visited + it))
+    private fun Graph<String>.pathsFrom(from: PathFragment): Long =
+        with(cache) {
+            getOrPut(from) {
+                when (from.currentNode) {
+                    "end" -> 1
+                    else -> (neighborsOf(from.currentNode) - "start").sumOf {
+                        when {
+                            it.all { char -> char.isUpperCase() } || it !in from.visited ->
+                                pathsFrom(from.copy(currentNode = it, visited = from.visited + it))
+                            it.all { char -> char.isLowerCase()  } && it in from.visited && !from.allowedToVisitSmallCaveAgain ->
+                                0L
+                            it.all { char -> char.isLowerCase()  } && it in from.visited && from.allowedToVisitSmallCaveAgain ->
+                                pathsFrom(from.copy(currentNode = it, visited = from.visited + it, allowedToVisitSmallCaveAgain = false))
+                            else ->
+                                pathsFrom(from.copy(currentNode = it, visited = from.visited + it))
+                        }
+                    }
                 }
             }
         }
-    }
 
     override fun part1(input: InputRepresentation): Long {
         val graph = input.asGraph()
         return graph.pathsFrom(PathFragment("start", graph.hashCode()))
     }
 
-    override fun part2(input: InputRepresentation): Any {
-        TODO("Not yet implemented")
+    override fun part2(input: InputRepresentation): Long {
+        val graph = input.asGraph()
+        return graph.pathsFrom(PathFragment("start", graph.hashCode(), true))
     }
 }
 
--- advent2021/src/test/kotlin/year2021/Day12Test.kt
+++ advent2021/src/test/kotlin/year2021/Day12Test.kt
@@ -18,4 +18,17 @@ class Day12Test {
         @Test
         fun `example 3`() = assertEquals(226L, Day12.testPart1("example3"))
     }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `example 1`() = assertEquals(36L, Day12.testPart2("example1"))
+
+        @Test
+        fun `example 2`() = assertEquals(103L, Day12.testPart2("example2"))
+
+        @Test
+        fun `example 3`() = assertEquals(3509L, Day12.testPart2("example3"))
+    }
 }
