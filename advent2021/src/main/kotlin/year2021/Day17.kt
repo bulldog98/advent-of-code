2eb7ee64cee5ac3de6f3cbe4e2554e250bc17246
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-03-09 14:28:49 +0100

    refactor(2021): day 17

--- advent2021/src/main/kotlin/year2021/Day17.kt
+++ advent2021/src/main/kotlin/year2021/Day17.kt
@@ -9,7 +9,28 @@ import kotlin.math.sign
 
 object Day17 : AdventDay(2021, 17) {
     // x's positive, y's negative
-    data class TargetArea(val xRange: LongRange, val yRange: LongRange) {
+    private data class TargetArea(val xRange: LongRange, val yRange: LongRange) {
+        private data class ProbePosition(val position: Point2D, val velocity: Point2D) {
+            private fun nextStep(): ProbePosition = copy(
+                position = position + velocity,
+                velocity = Point2D(
+                    x = velocity.x - velocity.x.sign,
+                    y = velocity.y - 1
+                )
+            )
+
+            fun simulate(targetArea: TargetArea) = generateSequence(this) { it.nextStep() }
+                .takeWhile { it.position.x <= targetArea.xRange.last && it.position.y >= targetArea.yRange.first }
+        }
+
+        fun computeAllSolutions() = (0..xRange.last).asSequence().flatMap { x ->
+            (yRange.first..yRange.first.absoluteValue).map { y ->
+                Point2D(x, y)
+            }
+        }.map { ProbePosition(Point2D.ORIGIN, it).simulate(this) }
+            .filter { it.any { p -> p.position.x in xRange && p.position.y in yRange } }
+            .map { it.map { p -> p.position } }
+
         companion object {
             fun parse(input: String) = input.toAllLongs().toList().let { (x1, x2, y1, y2) ->
                 TargetArea(x1..x2, y1..y2)
@@ -17,46 +38,13 @@ object Day17 : AdventDay(2021, 17) {
         }
     }
 
-    data class ProbePosition(val position: Point2D, val velocity: Point2D) {
-        fun nextStep(): ProbePosition = copy(
-            position = position + velocity,
-            velocity = Point2D(
-                x = velocity.x - velocity.x.sign,
-                y = velocity.y - 1
-            )
-        )
-
-        fun simulate(targetArea: TargetArea) = generateSequence(this) { it.nextStep() }
-            .takeWhile { it.position.x <= targetArea.xRange.last && it.position.y >= targetArea.yRange.first }
-    }
-
-    override fun part1(input: InputRepresentation): Long {
-        val targetArea = TargetArea.parse(input.asText())
-        val velocities = (0..targetArea.xRange.last).asSequence().flatMap { x ->
-            (targetArea.yRange.first..targetArea.yRange.first.absoluteValue).map { y ->
-                Point2D(x, y)
-            }
+    override fun part1(input: InputRepresentation): Long = TargetArea.parse(input.asText())
+        .computeAllSolutions().maxOf {
+            it.maxOf { p -> p.y }
         }
-        return velocities.map { ProbePosition(Point2D.ORIGIN, it).simulate(targetArea) }.maxOf {
-            val path = it.toList()
-            if (path.any { p -> p.position.x in targetArea.xRange && p.position.y in targetArea.yRange })
-                path.maxOf { p -> p.position.y }
-            else
-                Long.MIN_VALUE
-        }
-    }
 
-    override fun part2(input: InputRepresentation): Long {
-        val targetArea = TargetArea.parse(input.asText())
-        val velocities = (0..targetArea.xRange.last).asSequence().flatMap { x ->
-            (targetArea.yRange.first..targetArea.yRange.first.absoluteValue).map { y ->
-                Point2D(x, y)
-            }
-        }
-        return velocities.map { ProbePosition(Point2D.ORIGIN, it).simulate(targetArea) }.count {
-            it.any { p -> p.position.x in targetArea.xRange && p.position.y in targetArea.yRange }
-        }.toLong()
-    }
+    override fun part2(input: InputRepresentation): Long = TargetArea.parse(input.asText())
+        .computeAllSolutions().count().toLong()
 }
 
 fun main() = Day17.run()
