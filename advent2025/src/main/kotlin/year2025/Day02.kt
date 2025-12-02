69388e1c53327fd6411a6e286a410753eed656e3
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-02 07:18:36 +0100

    feat(2025): solve day 2 part 2

--- advent2025/src/main/kotlin/year2025/Day02.kt
+++ advent2025/src/main/kotlin/year2025/Day02.kt
@@ -3,7 +3,7 @@ package year2025
 import adventday.AdventDay
 import adventday.InputRepresentation
 
-private fun String.hasRepeatPattern(): Boolean {
+private fun String.isRepeatPattern(): Boolean {
     var i = 1
     while (i <= length / 2) {
         if (drop(i) in dropLast(length - i)) {
@@ -14,15 +14,35 @@ private fun String.hasRepeatPattern(): Boolean {
     return false
 }
 
+private fun String.isOnlyRepeatingSubstring(): Boolean {
+    var i = 1
+    while (i <= length / 2) {
+        if (take(i) in drop(i)) {
+            var string = take(i)
+            while (string.length < length) {
+                string += take(i)
+            }
+            if (string == this) {
+                return true
+            }
+        }
+        i++
+    }
+    return false
+}
+
 object Day02 : AdventDay(2025, 2) {
     override fun part1(input: InputRepresentation): Long = input[0]
         .split(",")
         .flatMap { it.split("-").let { (a, b) -> (a.toLong()..b.toLong()) } }
-        .filter { it.toString().hasRepeatPattern() }
+        .filter { it.toString().isRepeatPattern() }
         .sumOf { it }
 
-    override fun part2(input: InputRepresentation): Int =
-        TODO("Not yet implemented")
+    override fun part2(input: InputRepresentation): Long = input[0]
+        .split(",")
+        .flatMap { it.split("-").let { (a, b) -> (a.toLong()..b.toLong()) } }
+        .filter { it.toString().isOnlyRepeatingSubstring() }
+        .sumOf { it }
 }
 
 fun main() = Day02.run()
--- advent2025/src/test/kotlin/year2025/Day02Test.kt
+++ advent2025/src/test/kotlin/year2025/Day02Test.kt
@@ -8,4 +8,7 @@ class Day02Test {
 
     @Test
     fun part1() = assertEquals(1227775554L, day.testPart1())
+
+    @Test
+    fun part2() = assertEquals(4174379265L, day.testPart2())
 }
