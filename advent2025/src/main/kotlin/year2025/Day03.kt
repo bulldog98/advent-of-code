bf356426100767923a6c872d50176084b2d203c0
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-03 06:58:09 +0100

    feat(2025): solve day 3 part 2

--- advent2025/src/main/kotlin/year2025/Day03.kt
+++ advent2025/src/main/kotlin/year2025/Day03.kt
@@ -3,9 +3,16 @@ package year2025
 import adventday.AdventDay
 import adventday.InputRepresentation
 
+private fun List<Long>.turnOn(toTurnOn: Int, numberPrefix: Long = 0): Long = when (toTurnOn) {
+    0 -> numberPrefix
+    else -> dropLast(toTurnOn - 1).max().let { nextBiggest ->
+        drop(indexOf(nextBiggest) + 1).turnOn(toTurnOn - 1, numberPrefix * 10 + nextBiggest)
+    }
+}
+
 object Day03 : AdventDay(2025, 3) {
-    override fun part1(input: InputRepresentation): Int = input
-        .map { bank -> bank.toCharArray().map { "$it".toInt() } }
+    override fun part1(input: InputRepresentation): Long = input
+        .map { bank -> bank.toCharArray().map { "$it".toLong() } }
         .sumOf {
             val largestDigit = it.dropLast(1).max()
             val secondLargestDigit = it.drop(it.indexOf(largestDigit) + 1).max()
@@ -13,9 +20,11 @@ object Day03 : AdventDay(2025, 3) {
             largestDigit * 10 + secondLargestDigit
         }
 
-    override fun part2(input: InputRepresentation): Any {
-        TODO("Not yet implemented")
-    }
+    override fun part2(input: InputRepresentation): Long = input
+        .map { bank -> bank.toCharArray().map { "$it".toLong() } }
+        .sumOf { bank ->
+            bank.turnOn(12)
+        }
 }
 
 fun main() = Day03.run()
--- advent2025/src/test/kotlin/year2025/Day03Test.kt
+++ advent2025/src/test/kotlin/year2025/Day03Test.kt
@@ -7,5 +7,8 @@ class Day03Test {
     private val day = Day03
 
     @Test
-    fun part1() = assertEquals(357, day.testPart1())
+    fun part1() = assertEquals(357L, day.testPart1())
+
+    @Test
+    fun part2() = assertEquals(3121910778619L, day.testPart2())
 }
