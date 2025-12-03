a9ee73d2fef8ef11b3fab5011d0ad19c54dda5fa
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-03 06:59:33 +0100

    refactor(2025): day 3

--- advent2025/src/main/kotlin/year2025/Day03.kt
+++ advent2025/src/main/kotlin/year2025/Day03.kt
@@ -13,11 +13,8 @@ private fun List<Long>.turnOn(toTurnOn: Int, numberPrefix: Long = 0): Long = whe
 object Day03 : AdventDay(2025, 3) {
     override fun part1(input: InputRepresentation): Long = input
         .map { bank -> bank.toCharArray().map { "$it".toLong() } }
-        .sumOf {
-            val largestDigit = it.dropLast(1).max()
-            val secondLargestDigit = it.drop(it.indexOf(largestDigit) + 1).max()
-
-            largestDigit * 10 + secondLargestDigit
+        .sumOf { bank ->
+            bank.turnOn(2)
         }
 
     override fun part2(input: InputRepresentation): Long = input
