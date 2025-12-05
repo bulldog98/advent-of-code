e9a75459aab5d9c6ac0a9831228d8ec1a4970005
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-05 20:59:50 +0100

    refactor(2025): day 5

--- advent2025/src/main/kotlin/year2025/Day05.kt
+++ advent2025/src/main/kotlin/year2025/Day05.kt
@@ -4,23 +4,20 @@ import adventday.AdventDay
 import adventday.InputRepresentation
 import helper.pair.mapFirst
 import helper.pair.mapSecond
-import kotlin.math.max
-import kotlin.math.min
 
 private fun InputRepresentation.toRangesAndIngredients(): Pair<List<LongRange>, List<Long>> = asTwoBlocks()
     .mapFirst {
         it.lines.map { line: String ->
             val (i, j) = line.split("-")
             i.toLong()..j.toLong()
-        }
+        }.sortedBy { it.first }
     }
     .mapSecond { line ->
         line.lines.map { it: String -> it.toLong() }
     }
 
-fun LongRange.overlap(other: LongRange) = !(last < other.first || other.last < first)
-
-fun LongRange.merge(other: LongRange) = min(other.first, first)..max(other.last, last)
+private val LongRange.size: Long
+    get() = last - start + 1
 
 object Day05 : AdventDay(2025, 5) {
     override fun part1(input: InputRepresentation): Int {
@@ -28,23 +25,25 @@ object Day05 : AdventDay(2025, 5) {
         return rest.count { ranges.any { range -> it in range } }
     }
 
-    override fun part2(input: InputRepresentation): Long {
-        val (ranges) = input.toRangesAndIngredients()
-        val sortedRanges = ranges.sortedBy { it.first }
-        val mergedRanges = sortedRanges.toMutableSet()
-        while (true) {
-            val rangeA = mergedRanges.firstOrNull {
-                mergedRanges.any { other -> other != it && it.overlap(other) }
-            } ?: break
-            val rangeB = mergedRanges.first { it != rangeA && it.overlap(rangeA) }
-            mergedRanges.remove(rangeA)
-            mergedRanges.remove(rangeB)
-            mergedRanges.add(rangeA.merge(rangeB))
+    // use fold and sortedList
+    override fun part2(input: InputRepresentation): Long = input
+        .toRangesAndIngredients()
+        .first
+        .fold(emptyList<LongRange>()) { acc, next ->
+            val previous = acc.lastOrNull() ?: return@fold listOf(next)
+            when { // A: previous, B: next
+                // [A...] [B...] they do not touch
+                next.first > previous.last -> acc.plusElement(next)
+                // [A---B==)... overlapping or touching
+                next.last > previous.last -> acc.dropLast(1).plusElement(previous.first..next.last)
+                // B completely within A
+                else -> acc
+            }
         }
-        return mergedRanges.sumOf {
-            it.last - it.first + 1L
+        .sumOf {
+            it.size
         }
-    }
+
 }
 
 fun main() = Day05.run()
