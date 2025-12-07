886aba4630c5a2796fd1d2c0d0b1dcb8425049fe
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-07 09:09:37 +0100

    refactor(2025): simplify day 3

--- advent2025/src/main/kotlin/year2025/Day03.kt
+++ advent2025/src/main/kotlin/year2025/Day03.kt
@@ -10,18 +10,20 @@ private fun List<Long>.turnOn(toTurnOn: Int, numberPrefix: Long = 0): Long = whe
     }
 }
 
+private fun String.asBank() = map { it.digitToInt().toLong() }
+
 object Day03 : AdventDay(2025, 3, "Lobby") {
-    override fun part1(input: InputRepresentation): Long =
-        input
-            .lines.map { bank: String -> bank.toCharArray().map { "$it".toLong() } }
-            .sumOf { bank ->
+    override fun part1(input: InputRepresentation): Long = input
+        .lines
+        .map { it.asBank() }
+        .sumOf { bank ->
             bank.turnOn(2)
         }
 
-    override fun part2(input: InputRepresentation): Long =
-        input
-            .lines.map { bank: String -> bank.toCharArray().map { "$it".toLong() } }
-            .sumOf { bank ->
+    override fun part2(input: InputRepresentation): Long = input
+        .lines
+        .map { it.asBank() }
+        .sumOf { bank ->
             bank.turnOn(12)
         }
 }
