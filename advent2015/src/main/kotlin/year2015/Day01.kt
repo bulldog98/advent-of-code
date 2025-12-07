add7a099f7642fe69138734623e45e1b9015824c
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-07 11:59:01 +0100

    refactor(2015): day 1

--- advent2015/src/main/kotlin/year2015/Day01.kt
+++ advent2015/src/main/kotlin/year2015/Day01.kt
@@ -4,23 +4,23 @@ import adventday.AdventDay
 import adventday.InputRepresentation
 
 object Day01 : AdventDay(2015, 1, "Not Quite Lisp") {
-    override fun part1(input: InputRepresentation): Long =
-        input.lines[0].sumOf {
-            when (it) {
-                '(' -> 1L
-                ')' -> -1L
-                else -> error("not able to parse")
-            }
+    private fun Char.toFloorDifference() = when (this) {
+        '(' -> 1
+        ')' -> -1
+        else -> error("not able to parse")
+    }
+
+    override fun part1(input: InputRepresentation): Int = input
+        .lines[0]
+        .sumOf {
+            it.toFloorDifference()
         }
 
-    override fun part2(input: InputRepresentation): Int =
-        input.lines[0].runningFold(0L) { acc, it ->
-            acc + when (it) {
-                '(' -> 1L
-                ')' -> -1L
-                else -> error("not able to parse")
-            }
-        }.indexOfFirst { it == -1L }
+    override fun part2(input: InputRepresentation): Int = input
+        .lines[0]
+        .runningFold(0) { acc, it ->
+            acc + it.toFloorDifference()
+        }.indexOfFirst { it == -1 }
 }
 
 fun main() = Day01.run()
--- advent2015/src/test/kotlin/year2015/Day01Test.kt
+++ advent2015/src/test/kotlin/year2015/Day01Test.kt
@@ -2,14 +2,49 @@ package year2015
 
 import adventday.InputRepresentation
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class Day01Test {
     val day = Day01
 
-    @Test
-    fun part1() = assertEquals(0L, day.part1(InputRepresentation("(())")))
+    @Nested
+    @DisplayName("part1")
+    inner class Part1 {
+        @ValueSource(strings = ["(())", "()()"])
+        @ParameterizedTest
+        fun `end floor is floor 0`(instructions: String) =
+            assertEquals(0, day.part1(InputRepresentation(instructions)))
 
-    @Test
-    fun part2() = assertEquals(5, day.part2(InputRepresentation("()())")))
+        @ValueSource(strings = ["(((", "(()(()(", "))((((("])
+        @ParameterizedTest
+        fun `end floor is floor 3`(instructions: String) =
+            assertEquals(3, day.part1(InputRepresentation(instructions)))
+
+        @ValueSource(strings = ["())", "))("])
+        @ParameterizedTest
+        fun `end floor is floor -1`(instructions: String) =
+            assertEquals(-1, day.part1(InputRepresentation(instructions)))
+
+        @ValueSource(strings = [")))", ")())())"])
+        @ParameterizedTest
+        fun `end floor is floor -3`(instructions: String) =
+            assertEquals(-3, day.part1(InputRepresentation(instructions)))
+    }
+
+    @Nested
+    @DisplayName("part2")
+    inner class Part2 {
+        @Test
+        fun `first position to reach basement is 1`() =
+            assertEquals(1, day.part2(InputRepresentation(")")))
+
+
+        @Test
+        fun `first position to reach basement is 5`() =
+            assertEquals(5, day.part2(InputRepresentation("()())")))
+    }
 }
