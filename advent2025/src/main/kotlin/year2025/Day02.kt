53431ba47845910a5ca862944725edfac78e1b0f
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-07 09:06:41 +0100

    refactor(2025): simplify day 2

--- advent2025/src/main/kotlin/year2025/Day02.kt
+++ advent2025/src/main/kotlin/year2025/Day02.kt
@@ -3,6 +3,8 @@ package year2025
 import adventday.AdventDay
 import adventday.InputRepresentation
 
+private fun String.toLongRange() = split("-").let { (a, b) -> a.toLong()..b.toLong() }
+
 private fun String.isRepeatPattern(): Boolean = length / 2 * 2 == length &&
     take(length / 2 ) + take(length / 2) == this
 
@@ -20,13 +22,13 @@ private fun String.isOnlyRepeatingSubstring(): Boolean = (1 ..< length).any {
 object Day02 : AdventDay(2025, 2, "Gift Shop") {
     override fun part1(input: InputRepresentation): Long = input.lines[0]
         .split(",")
-        .flatMap { it.split("-").let { (a, b) -> (a.toLong()..b.toLong()) } }
+        .flatMap { it.toLongRange() }
         .filter { it.toString().isRepeatPattern() }
         .sumOf { it }
 
     override fun part2(input: InputRepresentation): Long = input.lines[0]
         .split(",")
-        .flatMap { it.split("-").let { (a, b) -> (a.toLong()..b.toLong()) } }
+        .flatMap { it.toLongRange() }
         .filter { it.toString().isOnlyRepeatingSubstring() }
         .sumOf { it }
 }
