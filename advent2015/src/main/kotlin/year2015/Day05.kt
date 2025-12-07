e9ac0d95f9fbd0afce34360b9d17e4821df84219
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-07 16:09:24 +0100

    refactor(2015): day 5

--- advent2015/src/main/kotlin/year2015/Day05.kt
+++ advent2015/src/main/kotlin/year2015/Day05.kt
@@ -9,34 +9,61 @@ import year2015.Day05.component3
 private val vowels = "aeiou".toSet()
 private val badStrings = setOf("ab", "cd", "pq", "xy")
 
+// typealias so we have parameter names
+private typealias Rule = (word: String) -> Boolean
+
+private fun String.allRulesApply(vararg rules: Rule): Boolean = rules.all { rule ->
+    rule(this)
+}
+
+private fun InputRepresentation.countNiceStrings(vararg rules: Rule): Int = lines
+    .count {
+        it.allRulesApply(*rules)
+    }
+
 object Day05 : AdventDay(2015, 5, "") {
     private operator fun String.component1() = this[0]
     private operator fun String.component2() = this[1]
     private operator fun String.component3() = this[2]
 
-    private fun String.has2CharsThatAreTwiceInWithoutOverlap(): Boolean =
-        indices
-            .take(length - 2)
+    private data object Has3Vowels : Rule {
+        override fun invoke(word: String): Boolean = word.count { c -> c in vowels } >= 3
+    }
+
+    private data object AnyCharRepeatsDirectly : Rule {
+        override fun invoke(word: String): Boolean = word.windowed(2).any { (a, b) -> a == b }
+    }
+
+    private data object NoBadStrings : Rule {
+        override fun invoke(word: String): Boolean = word.windowed(2).all { it !in badStrings }
+    }
+
+    private data object PairOfCharsIsRepeatedWithoutOverlap : Rule {
+        override fun invoke(word: String): Boolean = word.indices
+            // pair can only start at 4 last positon
+            .take(word.length - 4)
             .any {
-                substring(it, it + 2) in substring(it + 2).windowed(2)
+                // 2 chars, repeat
+                word.substring(it, it + 2) in word.substring(it + 2).windowed(2)
             }
+    }
+
+    private data object AnyCharRepeatsDirectlyWithCharBetween : Rule {
+        override fun invoke(word: String): Boolean = word.windowed(3).any { (a, _, b) -> a == b }
+    }
 
     override fun part1(input: InputRepresentation): Int = input
-        .lines
-        .count { line ->
-            line.count { c ->  c in vowels } >= 3 &&
-                line.windowed(2).any { (a, b) -> a == b } &&
-                line.windowed(2).all { it !in badStrings }
-        }
+        .countNiceStrings(
+            Has3Vowels,
+            AnyCharRepeatsDirectly,
+            NoBadStrings
+        )
 
     override fun part2(input: InputRepresentation): Int = input
-        .lines
-        .count { line ->
-            // any 2 chars, that are twice in the input (no overlap)
-            line.has2CharsThatAreTwiceInWithoutOverlap() &&
-                // any doubling with char in between
-                line.windowed(3).any { (a, _, b) -> a == b }
-        }
+        .countNiceStrings(
+            PairOfCharsIsRepeatedWithoutOverlap,
+            AnyCharRepeatsDirectlyWithCharBetween
+        )
 }
 
 fun main() = Day05.run()
