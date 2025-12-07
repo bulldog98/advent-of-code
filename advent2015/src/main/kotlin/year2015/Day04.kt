6b0934d9789bd860a83316ed87ba7799f317127c
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-07 12:42:25 +0100

    feat(2015): solve day 4 part 2

--- advent2015/src/main/kotlin/year2015/Day04.kt
+++ advent2015/src/main/kotlin/year2015/Day04.kt
@@ -1,6 +1,5 @@
 package year2015
 
-import NotImplementedYet
 import adventday.AdventDay
 import adventday.InputRepresentation
 import md5
@@ -11,8 +10,10 @@ object Day04 : AdventDay(2015, 4, "The Ideal Stocking Stuffer") {
             (input.lines[0] + "$it").md5().take(5).all { it == '0' }
         }
 
-    override fun part2(input: InputRepresentation): Any =
-        NotImplementedYet
+    override fun part2(input: InputRepresentation): Int =
+        (0..Int.MAX_VALUE).first {
+            (input.lines[0] + "$it").md5().take(6).all { it == '0' }
+        }
 }
 
 fun main() = Day04.run()
