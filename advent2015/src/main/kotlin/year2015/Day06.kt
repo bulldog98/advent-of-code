1cc7359d2ba17b3fb0b21d7746e68cec478d64cc
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-08 16:22:58 +0100

    refactor(2015): day 6

--- advent2015/src/main/kotlin/year2015/Day06.kt
+++ advent2015/src/main/kotlin/year2015/Day06.kt
@@ -2,8 +2,9 @@ package year2015
 
 import adventday.AdventDay
 import adventday.InputRepresentation
-import year2015.day06.Instruction
 import year2015.day06.Range
+
+typealias Instruction = (Int) -> Int
 
 class Grid {
     private val backingArray = Array(1000) { Array(1000) { 0 } }
@@ -19,7 +20,6 @@ class Grid {
     fun countBrightness(): Int = backingArray.sumOf { row -> row.sumOf { it } }
 }
 
-
 object Day06 : AdventDay(2015, 6, "Probably a Fire Hazard") {
     private fun InputRepresentation.computeBrightnessWithParser(instructionParser: (String) -> Instruction): Int =
         lines
@@ -32,10 +32,24 @@ object Day06 : AdventDay(2015, 6, "Probably a Fire Hazard") {
             }.countBrightness()
 
     override fun part1(input: InputRepresentation): Int = input
-        .computeBrightnessWithParser(Instruction::parsePart1)
+        .computeBrightnessWithParser {
+            when (it.trim()) {
+                "turn on" -> { _ -> 1 }
+                "turn off" -> { _ -> 0 }
+                "toggle" -> { brightness -> if (brightness == 0) 1 else 0 }
+                else -> error("Unknown instruction: $it")
+            }
+        }
 
     override fun part2(input: InputRepresentation): Int = input
-        .computeBrightnessWithParser(Instruction::parsePart2)
+        .computeBrightnessWithParser {
+            when (it.trim()) {
+                "turn on" -> { brightness -> brightness + 1 }
+                "turn off" -> { brightness -> (brightness - 1).coerceAtLeast(0) }
+                "toggle" -> { brightness -> brightness + 2 }
+                else -> error("Unknown instruction: $it")
+            }
+        }
 }
 
 fun main() = Day06.run()
