3a263c2fb91e6f6ce980db1f1ef2bedd28d2bba7
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-10 14:58:26 +0100

    feat(2015): solve day 12 part 2

--- advent2015/src/main/kotlin/year2015/Day12.kt
+++ advent2015/src/main/kotlin/year2015/Day12.kt
@@ -1,18 +1,31 @@
 package year2015
 
-import NotYetImplemented
 import adventday.AdventDay
 import adventday.InputRepresentation
-import helper.numbers.toAllLongs
+import kotlinx.serialization.json.*
 
 object Day12 : AdventDay(2015, 12, "JSAbacusFramework.io") {
-    override fun part1(input: InputRepresentation): Long =
-        input.text
-            .toAllLongs()
-            .sum()
+    fun String.parse() = Json.decodeFromString<JsonElement>(this)
 
-    override fun part2(input: InputRepresentation): Any =
-        NotYetImplemented
+    fun JsonElement.sumUp(isValid: (JsonElement) -> Boolean = { true }): Long = when {
+        !isValid(this) -> 0L
+        this is JsonArray -> sumOf { it.sumUp(isValid) }
+        this is JsonObject -> values.sumOf { it.sumUp(isValid) }
+        this is JsonPrimitive && !this.isString -> this.content.toLong()
+        else -> 0L
+    }
+
+    override fun part1(input: InputRepresentation): Long = input
+        .lines[0]
+        .parse()
+        .sumUp()
+
+    override fun part2(input: InputRepresentation): Long = input
+        .lines[0]
+        .parse()
+        .sumUp {
+            it !is JsonObject || !it.values.any { value -> value is JsonPrimitive && value.content == "red" }
+        }
 }
 
 fun main() = Day12.run()
--- advent2015/src/test/kotlin/year2015/Day12Test.kt
+++ advent2015/src/test/kotlin/year2015/Day12Test.kt
@@ -1,11 +1,24 @@
 package year2015
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class Day12Test {
     val day = Day12
 
+    // first example of part 1
     @Test
     fun part1() = assertEquals(6L, day.testPart1("example1.1"))
+
+    @Nested
+    @DisplayName("part2")
+    inner class Part2 {
+        @Test
+        fun `same example as part1`() = assertEquals(6L, day.testPart2("example1.1"))
+
+        @Test
+        fun `2nd part 2 example`() = assertEquals(4L, day.testPart2("example2.1"))
+    }
 }
