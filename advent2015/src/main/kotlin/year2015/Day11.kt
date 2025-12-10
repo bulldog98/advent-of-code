7f056f9c2e1d471fead1eb1b83bf297f12340acc
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-10 14:14:37 +0100

    feat(2015): solve day 11 part 2

--- advent2015/src/main/kotlin/year2015/Day11.kt
+++ advent2015/src/main/kotlin/year2015/Day11.kt
@@ -1,6 +1,5 @@
 package year2015
 
-import NotYetImplemented
 import adventday.AdventDay
 import adventday.InputRepresentation
 import year2015.Day11.component1
@@ -17,29 +16,36 @@ object Day11 : AdventDay(2015, 11, "Corporate Policy") {
     operator fun String.component2() = this[1]
     operator fun String.component3() = this[2]
 
+    fun String.passwordIsValid(): Boolean =
+        // three increasing chars in a row (no gaps)
+        this.windowed(3).any { (a, b, c) -> a.inc() == b && b.inc() == c } &&
+            // no forbidden letters
+            this.none { it in setOf('i', 'o', 'l') } &&
+            // 2 double letters non overlapping
+            this.take(3).let { (a, b, c) ->
+                // take first 2 into account
+                if (a == b && b != c)
+                    1
+                else
+                    0
+            } + this.windowed(3).count { (a, b, c) ->
+            a != b && b == c
+        } >= 2
+
     override fun part1(input: InputRepresentation): String = generateSequence(input.lines[0]) {
         it.inc()
     }
         .drop(1)
-        .first {
-            // three increasing chars in a row (no gaps)
-            it.windowed(3).any { (a, b, c) -> a.inc() == b && b.inc() == c } &&
-                // no forbidden letters
-                it.none { it in setOf('i', 'o', 'l') } &&
-                // 2 double letters non overlapping
-                it.take(3).let { (a, b, c) ->
-                    // take first 2 into account
-                    if (a == b && b != c)
-                        1
-                    else
-                        0
-                } + it.windowed(3).count { (a, b, c) ->
-                    a != b && b == c
-                } >= 2
-        }
+        .first { it.passwordIsValid() }
 
-    override fun part2(input: InputRepresentation): Any =
-        NotYetImplemented
+    override fun part2(input: InputRepresentation): String = generateSequence(input.lines[0]) {
+        it.inc()
+    }
+        .drop(1)
+        .filter { it.passwordIsValid() }
+        // expired again
+        .drop(1)
+        .first()
 }
 
 fun main() = Day11.run()
