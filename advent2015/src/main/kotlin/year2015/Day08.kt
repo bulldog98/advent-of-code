3d8095fd6364d527533d262f0e07d3f88edc4e58
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-10 09:57:29 +0100

    feat(2015): solve day 8 part 2

--- advent2015/src/main/kotlin/year2015/Day08.kt
+++ advent2015/src/main/kotlin/year2015/Day08.kt
@@ -1,12 +1,13 @@
 package year2015
 
-import NotYetImplemented
 import adventday.AdventDay
 import adventday.InputRepresentation
 
 object Day08 : AdventDay(2015, 8, "Matchsticks") {
     private val escapedBackspace = Regex("""\\\\""")
+    private val unescapedBackspace = Regex("""\\""")
     private val escapedQuote = Regex("""\\"""")
+    private val unescapedQuote = Regex("\"")
     private val escapedAsci = Regex("""\\x[\d,a-f][\d,a-f]""")
 
     override fun part1(input: InputRepresentation): Long = input
@@ -20,8 +21,17 @@ object Day08 : AdventDay(2015, 8, "Matchsticks") {
             chars - content.length
         }
 
-    override fun part2(input: InputRepresentation): Any =
-        NotYetImplemented
+    override fun part2(input: InputRepresentation): Long = input
+        .lines
+        .sumOf {
+            val chars = it.length.toLong()
+
+            val content = it
+                .replace(unescapedBackspace, "11")
+                .replace(unescapedQuote, "11")
+            // the quotes + actual escaped content - length of input
+            2 + content.length - chars
+        }
 }
 
 fun main() = Day08.run()
--- advent2015/src/test/kotlin/year2015/Day08Test.kt
+++ advent2015/src/test/kotlin/year2015/Day08Test.kt
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test
 class Day08Test {
     val day = Day08
 
+    // all examples in one file are the test file
+
     @Test
     fun part1() = assertEquals(12L, day.testPart1())
+
+    @Test
+    fun part2() = assertEquals(19L, day.testPart2())
 }
