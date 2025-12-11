593732f26656025fe029321d5a4ea8e86b831065
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-11 14:25:01 +0100

    refactor(2015): day 18

--- advent2015/src/main/kotlin/year2015/Day18.kt
+++ advent2015/src/main/kotlin/year2015/Day18.kt
@@ -18,6 +18,38 @@ class Day18(
         )
     }
 
+    private fun InputRepresentation.simulateAndCountLightsAfterEnd(
+        alwaysOnPoints: Set<Point2D> = emptySet()
+    ): Int = lines
+        .findAllPositionsOf()
+        .let {
+            generateSequence(it + alwaysOnPoints) { currentOn ->
+                buildSet {
+                    addAll(alwaysOnPoints)
+                    currentOn.forEach { on ->
+                        when (on.neighborHood.count { it in currentOn }) {
+                            2, 3 -> add(on)
+                            else -> {}
+                        }
+                    }
+                    for (x in 0..<gridSize) {
+                        for (y in 0..<gridSize) {
+                            val point = Point2D(x, y)
+                            if (point in currentOn) continue
+                            if (point.neighborHood.count { it in currentOn } == 3) add(point)
+                        }
+                    }
+                }
+            }
+            // disabled visual debug
+            //                .prettyPrint()
+        }
+        .drop(1) // ignore start configuration
+        .take(evaluateAfterXSteps)
+        .last()
+        .size
+
+    // region visual debug helpers
     fun Set<Point2D>.prettyPrint() = (0..<gridSize).forEach { y ->
         println((0..<gridSize).joinToString("") { x ->
             if (Point2D(x, y) in this) "#" else "."
@@ -37,63 +69,12 @@ class Day18(
             if (it.index < evaluateAfterXSteps) println()
             it.value
         }
+    // endregion
 
-    override fun part1(input: InputRepresentation): Int = input
-        .lines
-        .findAllPositionsOf()
-        .let {
-            generateSequence(it) { currentOn ->
-                buildSet {
-                    currentOn.forEach { on ->
-                        when (on.neighborHood.count { it in currentOn }) {
-                            2, 3 -> add(on)
-                            else -> {}
-                        }
-                    }
-                    for (x in 0..<gridSize) {
-                        for (y in 0..<gridSize) {
-                            val point = Point2D(x, y)
-                            if (point in currentOn) continue
-                            if (point.neighborHood.count { it in currentOn } == 3) add(point)
-                        }
-                    }
-                }
-            }
-//                .prettyPrint()
-        }
-        .drop(1) // ignore start configuration
-        .take(evaluateAfterXSteps)
-        .last()
-        .size
+    override fun part1(input: InputRepresentation): Int = input.simulateAndCountLightsAfterEnd()
 
     override fun part2(input: InputRepresentation): Int = input
-        .lines
-        .findAllPositionsOf()
-        .let {
-            generateSequence(it + fourCorners) { currentOn ->
-                buildSet {
-                    addAll(fourCorners)
-                    currentOn.forEach { on ->
-                        when (on.neighborHood.count { it in currentOn }) {
-                            2, 3 -> add(on)
-                            else -> {}
-                        }
-                    }
-                    for (x in 0..<gridSize) {
-                        for (y in 0..<gridSize) {
-                            val point = Point2D(x, y)
-                            if (point in currentOn) continue
-                            if (point.neighborHood.count { it in currentOn } == 3) add(point)
-                        }
-                    }
-                }
-            }
-//                .prettyPrint()
-        }
-        .drop(1) // ignore start configuration
-        .take(evaluateAfterXSteps)
-        .last()
-        .size
+        .simulateAndCountLightsAfterEnd(fourCorners)
 }
 
 fun main() = Day18(100, 100).run()
