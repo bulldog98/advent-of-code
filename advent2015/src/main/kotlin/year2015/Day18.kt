d2b54f2bf9f06869330a5ef8ddc43c93bcc34893
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-11 14:18:43 +0100

    feat(2015): solve day 18 part 2

--- advent2015/src/main/kotlin/year2015/Day18.kt
+++ advent2015/src/main/kotlin/year2015/Day18.kt
@@ -1,6 +1,5 @@
 package year2015
 
-import NotYetImplemented
 import Point2D
 import adventday.AdventDay
 import adventday.InputRepresentation
@@ -10,6 +9,35 @@ class Day18(
     val gridSize: Long,
     val evaluateAfterXSteps: Int
 ) : AdventDay(2015, 18, "Like a GIF For Your Yard") {
+    val fourCorners by lazy {
+        setOf(
+            Point2D.ORIGIN,
+            Point2D(0, gridSize - 1),
+            Point2D(gridSize - 1, 0),
+            Point2D(gridSize - 1, gridSize - 1),
+        )
+    }
+
+    fun Set<Point2D>.prettyPrint() = (0..<gridSize).forEach { y ->
+        println((0..<gridSize).joinToString("") { x ->
+            if (Point2D(x, y) in this) "#" else "."
+        })
+    }
+
+    @Suppress("unused")
+    fun Sequence<Set<Point2D>>.prettyPrint() = withIndex()
+        .map {
+            if (it.index == 0)
+                println("Initial state:")
+            else {
+                val pluralization = if (it.index > 1) "s" else ""
+                println("After ${it.index} step$pluralization:")
+            }
+            it.value.prettyPrint()
+            if (it.index < evaluateAfterXSteps) println()
+            it.value
+        }
+
     override fun part1(input: InputRepresentation): Int = input
         .lines
         .findAllPositionsOf()
@@ -31,14 +59,41 @@ class Day18(
                     }
                 }
             }
+//                .prettyPrint()
         }
         .drop(1) // ignore start configuration
         .take(evaluateAfterXSteps)
         .last()
         .size
 
-    override fun part2(input: InputRepresentation): Any =
-        NotYetImplemented
+    override fun part2(input: InputRepresentation): Int = input
+        .lines
+        .findAllPositionsOf()
+        .let {
+            generateSequence(it + fourCorners) { currentOn ->
+                buildSet {
+                    addAll(fourCorners)
+                    currentOn.forEach { on ->
+                        when (on.neighborHood.count { it in currentOn }) {
+                            2, 3 -> add(on)
+                            else -> {}
+                        }
+                    }
+                    for (x in 0..<gridSize) {
+                        for (y in 0..<gridSize) {
+                            val point = Point2D(x, y)
+                            if (point in currentOn) continue
+                            if (point.neighborHood.count { it in currentOn } == 3) add(point)
+                        }
+                    }
+                }
+            }
+//                .prettyPrint()
+        }
+        .drop(1) // ignore start configuration
+        .take(evaluateAfterXSteps)
+        .last()
+        .size
 }
 
 fun main() = Day18(100, 100).run()
--- advent2015/src/test/kotlin/year2015/Day18Test.kt
+++ advent2015/src/test/kotlin/year2015/Day18Test.kt
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day18Test {
-    val day = Day18(6, 4)
+    val dayWithEvaluateAfter4Steps = Day18(6, 4)
+    val dayWithEvaluateAfter5Steps = Day18(6, 5)
 
     @Test
-    fun part1() = assertEquals(4, day.testPart1())
+    fun part1() = assertEquals(4, dayWithEvaluateAfter4Steps.testPart1())
+
+    @Test
+    fun part2() = assertEquals(17, dayWithEvaluateAfter5Steps.testPart2())
 }
