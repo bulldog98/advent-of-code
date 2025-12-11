d4cac648644f914abbe9571cfb1491c316e07032
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-11 10:27:25 +0100

    refactor(2025): day 11
    improve readability

--- advent2025/src/main/kotlin/year2025/Day11.kt
+++ advent2025/src/main/kotlin/year2025/Day11.kt
@@ -6,40 +6,56 @@ import adventday.InputRepresentation
 object Day11 : AdventDay(2025, 11, "Reactor") {
     val splitRegex = Regex(""" |: """)
 
-    fun InputRepresentation.asLookupMap() = lines
+    fun <M> InputRepresentation.asGraphWithMemory(
+        memory: M,
+        isFinal: (M) -> Boolean = { _ -> true },
+        adjustMemory: (String, M) -> M = { _, memory -> memory },
+    ) = lines
         .map { it.split(splitRegex) }
         .associate { it[0] to it.drop(1) }
-
-    fun Map<String, List<String>>.countPathsFrom(
-        node: String,
-        visitedDac: Boolean,
-        visitedFft: Boolean,
-        visited: MutableMap<Triple<String, Boolean, Boolean>, Long> = mutableMapOf(),
-        conditionForValidPath: (Boolean, Boolean) -> Boolean = Boolean::and
-    ): Long =
-        visited.getOrPut(Triple(node, visitedDac, visitedFft)) {
-            fun countNextPaths(
-                visited: MutableMap<Triple<String, Boolean, Boolean>, Long>,
-                node: String,
-                visitedDac: Boolean,
-                visitedFft: Boolean,
-                conditionForValidPath: (Boolean, Boolean) -> Boolean
-            ) = this[node].orEmpty().sumOf { countPathsFrom(it, visitedDac, visitedFft, visited, conditionForValidPath) }
-            when (node) {
-                "dac" -> countNextPaths(visited, node, visitedDac = true, visitedFft, conditionForValidPath)
-                "fft" -> countNextPaths(visited, node, visitedDac, visitedFft = true, conditionForValidPath)
-                "out" if (conditionForValidPath(visitedDac, visitedFft)) -> 1L
-                else -> countNextPaths(visited, node, visitedDac, visitedFft, conditionForValidPath)
-            }
+        .let { graphWithoutMemory ->
+            GraphWithMemory(
+                graphWithoutMemory = graphWithoutMemory,
+                initialMemory = memory,
+                adjustMemory = adjustMemory,
+                isFinal = isFinal,
+            )
         }
 
+    data class GraphWithMemory<M>(
+        val graphWithoutMemory: Map<String, List<String>>,
+        val initialMemory: M,
+        val adjustMemory: (String, M) -> M = { _, memory -> memory },
+        val isFinal: (M) -> Boolean = { _ -> false },
+    ) {
+        fun countPathsFromToOut(from: String): Long = mutableMapOf<Pair<String, M>, Long>()
+            .memoizedCountPathsFromToOut(from, initialMemory)
+
+        private fun MutableMap<Pair<String, M>, Long>.memoizedCountPathsFromToOut(from: String, memory: M): Long =
+            getOrPut(from to memory) {
+                when (from) {
+                    "out" if (isFinal(memory)) -> 1L
+                    else -> graphWithoutMemory[from]
+                        .orEmpty()
+                        .sumOf { memoizedCountPathsFromToOut(it, adjustMemory(from, memory)) }
+                }
+            }
+    }
+
+    // part 1 was solvable without memoization
     override fun part1(input: InputRepresentation): Long = input
-        .asLookupMap()
-        .countPathsFrom("you", visitedDac = false, visitedFft = false) { _, _ -> true }
+        .asGraphWithMemory(1)
+        .countPathsFromToOut("you")
 
     override fun part2(input: InputRepresentation): Long = input
-        .asLookupMap()
-        .countPathsFrom("svr", visitedDac = false, visitedFft = false)
+        .asGraphWithMemory(false to false, { (a, b) -> a && b }) { node, (a, b) ->
+            when (node) {
+                "fft" -> a to true
+                "dac" -> true to b
+                else -> a to b
+            }
+        }
+        .countPathsFromToOut("svr")
 }
 
 fun main() = Day11.run()
