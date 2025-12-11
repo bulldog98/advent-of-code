9402486659bf12d5c7a2b33528d807cc01344f21
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2025-12-11 10:57:54 +0100

    feat(2015): solve day 15 part 1

---
+++ advent2015/src/main/kotlin/year2015/Day15.kt
@@ -0,0 +1,57 @@
+package year2015
+
+import NotYetImplemented
+import adventday.AdventDay
+import adventday.InputRepresentation
+import helper.numbers.toAllLongs
+
+object Day15 : AdventDay(2015, 15, "Science for Hungry People") {
+    data class Ingredient(
+        val name: String,
+        val capacity: Int,
+        val durability: Int,
+        val flavor: Int,
+        val texture: Int,
+        val calories: Int,
+    ) {
+        companion object {
+            fun parse(line: String): Ingredient {
+                val (capacity, durability, flavor, texture, calories) = line.toAllLongs().map { it.toInt() }.toList()
+                return Ingredient(
+                    line.split(": ")[0],
+                    capacity,
+                    durability,
+                    flavor,
+                    texture,
+                    calories,
+                )
+            }
+        }
+    }
+
+    fun partition(number: Int, parts: Int, partitions: List<List<Int>> = listOf(emptyList())): List<List<Int>> = when (parts) {
+        1 -> partitions.map { it + listOf(number) }
+        else -> (0..number).flatMap { numberChosen ->
+            partition(number - numberChosen, parts - 1, partitions.map { it + listOf(numberChosen) })
+        }
+    }
+
+    override fun part1(input: InputRepresentation): Long = input
+        .lines
+        .map(Ingredient::parse)
+        .let { ingredients ->
+            partition(100, ingredients.size).maxOf { parts ->
+                val list = parts.zip(ingredients)
+                val capacity = list.sumOf { (amount, ingredient) -> amount * ingredient.capacity }.coerceAtLeast(0)
+                val durability = list.sumOf { (amount, ingredient) -> amount * ingredient.durability }.coerceAtLeast(0)
+                val flavor = list.sumOf { (amount, ingredient) -> amount * ingredient.flavor }.coerceAtLeast(0)
+                val texture = list.sumOf { (amount, ingredient) -> amount * ingredient.texture }.coerceAtLeast(0)
+                capacity.toLong() * durability * flavor * texture
+            }
+        }
+
+    override fun part2(input: InputRepresentation): Any =
+        NotYetImplemented
+}
+
+fun main() = Day15.run()
---
+++ advent2015/src/test/kotlin/year2015/Day15Test.kt
@@ -0,0 +1,11 @@
+package year2015
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day15Test {
+    val day = Day15
+
+    @Test
+    fun part1() = assertEquals(62842880L, day.testPart1())
+}
